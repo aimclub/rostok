a5773492c65b443c7fca04b2d2963be236f42c40
KirillZharkov <rubekon13@gmail.com>
2022-07-25 19:21:27 +0300

    Add wrapper for terminal node

--- node_block_bind.py
+++ node_block_bind.py
@@ -1,3 +1,14 @@
 import node_render
 import node
 
+class NodeTerminal:
+    def __init__(self, block_cls, *args, **kwargs):
+        self.builder = None
+        self.block_cls = block_cls
+        self.args = args
+        self.kwargs = kwargs
+
+    def create_block(self):
+        if self.builder is None:
+            raise Exception('Set builder first')
+        return self.block_cls(self.builder, *self.args, **self.kwargs)
