2a04220d0f6f273a700ff0b7bf43325e542f30b0
ZharkovKirill <rubekon13@gmail.com>
2022-07-26 18:18:17 +0300

    Add rules and nodes for example

--- node_block_bind.py
+++ node_block_bind.py
@@ -12,3 +12,104 @@ class NodeTerminal:
         if self.builder is None:
             raise Exception('Set builder first')
         return self.block_cls(self.builder, *self.args, **self.kwargs)
+FlatCreate.id_node_connect_child = 0
+FlatCreate.id_node_connect_parent = 0
+FlatCreate.graph_insert = rule_graph
+FlatCreate.replaced_node = ROOT
+
+Mount = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=F)
+rule_graph.add_node(1, Node=M)
+rule_graph.add_node(2, Node=EM)
+rule_graph.add_edge(0, 1)
+rule_graph.add_edge(1, 2)
+Mount.id_node_connect_child = 0
+Mount.id_node_connect_parent = 2
+Mount.graph_insert = rule_graph
+Mount.replaced_node = F
+
+MountAdd = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=M)
+rule_graph.add_node(1, Node=EM)
+rule_graph.add_edge(0, 1)
+MountAdd.id_node_connect_child = 1
+MountAdd.id_node_connect_parent = 0
+MountAdd.graph_insert = rule_graph
+MountAdd.replaced_node = M
+
+FingerUpper = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=J)
+rule_graph.add_node(1, Node=L)
+rule_graph.add_node(2, Node=EM)
+rule_graph.add_edge(0, 1)
+rule_graph.add_edge(1, 2)
+FingerUpper.id_node_connect_child = 2
+FingerUpper.id_node_connect_parent = 0
+FingerUpper.graph_insert = rule_graph
+FingerUpper.replaced_node = EM
+
+# Terminal
+TerminalFlat = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=F1)
+TerminalFlat.id_node_connect_child = 0
+TerminalFlat.id_node_connect_parent = 0
+TerminalFlat.graph_insert = rule_graph
+TerminalFlat.replaced_node = F
+
+TerminalL1 = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=L1)
+TerminalL1.id_node_connect_child = 0
+TerminalL1.id_node_connect_parent = 0
+TerminalL1.graph_insert = rule_graph
+TerminalL1.replaced_node = L
+
+TerminalTransformR = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=T3)
+TerminalTransformR.id_node_connect_child = 0
+TerminalTransformR.id_node_connect_parent = 0
+TerminalTransformR.graph_insert = rule_graph
+TerminalTransformR.replaced_node = M
+
+TerminalTransformL = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=T4)
+TerminalTransformL.id_node_connect_child = 0
+TerminalTransformL.id_node_connect_parent = 0
+TerminalTransformL.graph_insert = rule_graph
+TerminalTransformL.replaced_node = M
+
+TerminalEndLimb = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=U1)
+TerminalEndLimb.id_node_connect_child = 0
+TerminalEndLimb.id_node_connect_parent = 0
+TerminalEndLimb.graph_insert = rule_graph
+TerminalEndLimb.replaced_node = EM
+
+TerminalJoint = Rule()
+rule_graph = nx.DiGraph()
+rule_graph.add_node(0, Node=J1)
+TerminalJoint.id_node_connect_child = 0
+TerminalJoint.id_node_connect_parent = 0
+TerminalJoint.graph_insert = rule_graph
+TerminalJoint.replaced_node = J
+
+G = Grammar()
+
+rule_action = [FlatCreate, Mount, Mount, FingerUpper, FingerUpper,
+               TerminalFlat, TerminalL1, TerminalL1, TerminalTransformR, TerminalTransformL, TerminalEndLimb,
+               TerminalEndLimb,
+               TerminalJoint, TerminalJoint]
+for i in rule_action:
+    G.apply_rule(i)
+
+mysystem = chrono.ChSystemNSC()
+
+wrapper_array = G.build_wrapper_array()
+print()
