1413879a469fc84ffc7c439b5395aaa4c74977db
Huowl <kraken.mix@gmail.com>
2022-09-19 12:50:48 +0300

    Add setter various track's form

--- control.py
+++ control.py
@@ -4,6 +4,7 @@ import scipy.interpolate as interpolate
 import numpy as np
 from abc import ABC
 import enum as Enum
+#from multipledispatch import dispatch
 
 from node_render import ChronoRevolveJoint
 
@@ -17,6 +18,15 @@ def define_inputs(joint,des_input,type):
     elif type is ChronoRevolveJoint.InputType.Uncontrol:
         print("Uncontrollable joint")
 
+
+def time_points_2_ChFunction(arr_time_point):
+    arr_time_point = arr_time_point.T
+    spline_coefficients = interpolate.splrep(arr_time_point[:,0],arr_time_point[:,1])
+    time_interval = (arr_time_point[0,0],arr_time_point[-1,0])
+    out_func = ChTrajectory(spline_coefficients,time_interval)
+
+    return out_func
+
 def points_2_ChFunction(des_points, time_interval: tuple):
     num_points  = np.size(des_points)
     traj_time = np.linspace(time_interval[0],time_interval[1],num_points)
@@ -25,6 +35,7 @@ def points_2_ChFunction(des_points, time_interval: tuple):
     out_func = ChTrajectory(spline_coefficients,time_interval)
 
     return out_func
+
 
 class ChTrajectory(chrono.ChFunction):
     def __init__(self, coefficients, time_interval):
@@ -43,46 +54,6 @@ class ChTrajectory(chrono.ChFunction):
             y = interpolate.splev(self.time_interval[1], self.coefficients)
         return float(y)
 
-'''
-class ChPID(chrono.ChFunction_SetpointCallback):
-    def __init__(self, joint, coeff_p, coeff_d, coeff_i = 0.):
-        super().__init__()
-        self.K_P = coeff_p
-        self.K_D = coeff_d
-        self.K_I = coeff_i
-        self.err_i = 0
-        self.joint = joint
-        self.prev_time = 0.
-        self.des_pos = 0.
-        self.des_vel = 0.
-        self.des_acc = 0.
-        self.F = 0
-
-    def Clone(self):
-        return deepcopy(self)
-
-    def set_des_point(self, des_position = 0., des_velocity = 0., des_acceleration = 0.):
-        self.des_pos = des_position
-        self.des_vel = des_velocity
-        self.des_acc = des_acceleration
-
-
-    def SetpointCallback(self, x):
-        time = x
-        if (self.prev_time < time):
-            mes_pos = self.joint.GetMotorRot()
-            err =  self.des_pos - mes_pos
-            
-            mes_vel = self.joint.GetMotorRot_dt()
-            d_err = self.des_vel - mes_vel
-
-            self.err_i += err*(time - self.prev_time)
-            
-            self.F = self.K_P*err + self.K_D*d_err + self.K_I*self.err_i
-            self.prev_time = time
-        return self.F
-
-'''
 class ChPID(chrono.ChFunction_SetpointCallback):
     def __init__(self, joint, coeff_p, coeff_d, coeff_i = 0.):
         super().__init__()
@@ -152,18 +123,18 @@ class TrackingControl(Control):
         self.time_interval = time_interval
         self.chr_trajectory = points_2_ChFunction(des_points,self.time_interval)
         self.set_input(self.chr_trajectory)
-'''
+
 class ChControllerPID(Control):
-    def __init__(self, joint_block, des_position, coeff_p, coeff_d, coeff_i = 0.):
+    def __init__(self, joint_block, coeff_p, coeff_d, coeff_i = 0.):
         Control.__init__(self,joint_block)
         self.PID_ctrl = ChPID(self.get_joint(),coeff_p, coeff_d, coeff_i)
-        self.PID_ctrl.set_des_point(des_position)
+
+    def set_des_trajectory_interval(self, des_pos : np.array, time_interval: tuple):
+        self.traj = points_2_ChFunction(des_pos,time_interval)
+        self.PID_ctrl.set_des_point(self.traj)
         self.set_input(self.PID_ctrl)
-'''
-class ChControllerPID(Control):
-    def __init__(self, joint_block, des_position, coeff_p, coeff_d, coeff_i = 0.):
-        Control.__init__(self,joint_block)
-        self.PID_ctrl = ChPID(self.get_joint(),coeff_p, coeff_d, coeff_i)
-        self.traj = points_2_ChFunction(des_position,(0.5, 5))
+
+    def set_des_trajectory(self, des_arr_time_to_pos: np.array):
+        self.traj = time_points_2_ChFunction(des_arr_time_to_pos)
         self.PID_ctrl.set_des_point(self.traj)
         self.set_input(self.PID_ctrl)
