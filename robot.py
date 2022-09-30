407837e05287431b82e3971a2b87271aa0c96444
ZharkovKirill <labradudel13@gmail.com>
2022-09-30 20:05:08 +0300

    [robot] Add declaration graph constructor

--- robot.py
+++ robot.py
@@ -3,9 +3,9 @@ from node_render import connect_blocks
 
 class Robot:
     def __init__(self, wrapper_tuple_array: list[list[WrapperTuple]], simulation):
-        self.wrapper_array = self.__build_robot(simulation,wrapper_tuple_array) 
-        self.graph_representation 
-    def __build_robot(self, simulation, wrapper_tuple_array: list[list[WrapperTuple]]) -> list[list[BlockWrapper]]:
+        self.block_map = self.__build_robot(simulation,wrapper_tuple_array) 
+
+    def __build_robot(self, simulation, wrapper_tuple_array: list[list[WrapperTuple]]):
         blocks = []
         uniq_blocks = {}
         for wrapper_tuple_line in wrapper_tuple_array:
@@ -29,4 +29,4 @@ class Robot:
         for line in blocks:
             connect_blocks(line)
         
-        return blocks
+        return uniq_blocks
