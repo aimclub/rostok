8097563b7afad402ac699060c3924cfc63782401
ZharkovKirill <labradudel13@gmail.com>
2022-10-05 18:17:05 +0300

    [graph_reward] Add example of usage

--- src/stubs/graph_reward.py
+++ src/stubs/graph_reward.py
@@ -3,29 +3,34 @@ from engine.node import Node, GraphGrammar
 
 # The deeper the node, the greater the reward
 def get_graph_sum_complex_reward(graph: GraphGrammar, reward_map: dict[Node, float]) -> float:
-    
     dfs = graph.graph_partition_dfs()
     sum = 0
     for branch in dfs:
         branch_len = len(branch)
         for number, nodes_id in enumerate(branch):
             node = graph.nodes.get(nodes_id)["Node"]
-            node_reward = reward_map[node]
+            try:
+                node_reward = reward_map[node]
+            except KeyError:
+                raise Exception(
+                    "There is no node labeled : {label} in reward_map".format(label=node.label))
             node_reward = node_reward * (number+1)/(branch_len+1)
             sum += node_reward
-    
+
     return sum
 
 
 def get_graph_mul_reward(graph: GraphGrammar, reward_map: dict[Node, float]) -> float:
-    
     dfs = graph.graph_partition_dfs()
-    mul  = 1
+    mul = 1
     for branch in dfs:
-        branch_len = len(branch)
         for number, nodes_id in enumerate(branch):
             node = graph.nodes.get(nodes_id)["Node"]
-            node_reward = reward_map[node]
+            try:
+                node_reward = reward_map[node]
+            except KeyError:
+                raise Exception(
+                    "There is no node labeled : {label} in reward_map".format(label=node.label))
             mul *= node_reward
-    
+
     return mul
