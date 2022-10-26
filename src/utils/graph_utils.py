8a888d9f6c5662e2ffe7a834c048ee7f42af302d
ZharkovKirill <labradudel13@gmail.com>
2022-10-26 19:28:03 +0300

    [graph_utils] Add node replace

---
+++ src/utils/graph_utils.py
@@ -0,0 +1,10 @@
+from engine.node import GraphGrammar, Node
+
+def replace_nodes(graph: GraphGrammar, mapping: dict[Node: Node]):
+    graph_dict = dict(graph.nodes.items())
+    for node_replace in mapping.keys():
+        id_replace = graph.find_nodes(node_replace)
+        for id in id_replace:
+            graph_dict[id]["Node"] = mapping[node_replace]
+
+
