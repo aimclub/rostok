6f59a971cbd3c53a65e1ac11e10e3a1f1ae8ba76
ZharkovKirill <labradudel13@gmail.com>
2022-11-07 22:20:54 +0300

    [example_simstep] Add iterate over all mechanism

--- examples/example_simstep.py
+++ examples/example_simstep.py
@@ -20,6 +20,12 @@ from example_ruleset import get_terminal_graph_ladoshaka, get_terminal_graph_two
 from utils.blocks_utils import NodeFeatures
 from numpy import arange
 
+"""
+    Example generate random constant torque
+    Calculate all info about joint from graph
+    
+"""
+
 
 def create_const_traj(torque_value, stop_time: float, time_step: float):
     timeseries_traj = []
@@ -43,56 +49,58 @@ def create_torque_traj_from_x(joint_dfs, x: list[float], stop_time: float, time_
 
     return torque_traj
 
+mechs = [get_terminal_graph_three_finger, get_terminal_graph_ladoshaka, get_terminal_graph_two_finger]
 
-G = get_terminal_graph_three_finger()
+for get_graph in mechs:
+    G = get_graph()
 
-dfs_patrion_ids = G.graph_partition_dfs()
-def get_node(node_id): return G.get_node_by_id(node_id)
-
-
-dfs_patrion_node = [[get_node(node_id) for node_id in branch]
-                    for branch in dfs_patrion_ids]
-dfs_j = []
-number_trq = 0
-for branch in dfs_patrion_node:
-    joint_branch = list(filter(NodeFeatures.is_joint, branch))
-    # Strange things, for dect empty list
-    # len([[]]) is 1
-    len_joints = len(joint_branch)
-    number_trq += len_joints
-    if len_joints != 0:
-        dfs_j.append(joint_branch)
+    dfs_patrion_ids = G.graph_partition_dfs()
+    def get_node(node_id): return G.get_node_by_id(node_id)
 
 
-const_torque_koef = [random.random() for _ in range(number_trq)]
-arr_trj = create_torque_traj_from_x(dfs_j, const_torque_koef, 10, 0.1)
+    dfs_patrion_node = [[get_node(node_id) for node_id in branch]
+                        for branch in dfs_patrion_ids]
+    dfs_j = []
+    number_trq = 0
+    for branch in dfs_patrion_node:
+        joint_branch = list(filter(NodeFeatures.is_joint, branch))
+        # Strange things, for dect empty list
+        # len([[]]) is 1
+        len_joints = len(joint_branch)
+        number_trq += len_joints
+        if len_joints != 0:
+            dfs_j.append(joint_branch)
 
 
-chrono_system = chrono.ChSystemNSC()
-grab_robot = robot.Robot(G, chrono_system)
-
-joints = np.array(grab_robot.get_joints)
+    const_torque_koef = [random.random() for _ in range(number_trq)]
+    arr_trj = create_torque_traj_from_x(dfs_j, const_torque_koef, 10, 0.1)
 
 
-config_sys = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
+    chrono_system = chrono.ChSystemNSC()
+    grab_robot = robot.Robot(G, chrono_system)
 
-time_to_contact = 2
-time_without_contact = 0.2
-max_time = 10
+    joints = np.array(grab_robot.get_joints)
 
 
-flags = [FlagMaxTime(max_time)]
+    config_sys = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
 
-times_step = 1e-3
+    time_to_contact = 2
+    time_without_contact = 0.2
+    max_time = 10
 
-grab_obj_mat = chrono.ChMaterialSurfaceNSC()
-grab_obj_mat.SetFriction(0.5)
-grab_obj_mat.SetDampingF(0.1)
-obj = chrono.ChBodyEasyBox(0.2, 0.2, 0.6, 1000, True, True, grab_obj_mat)
-obj.SetCollide(True)
-obj.SetPos(chrono.ChVectorD(0, 1.2, 0))
 
-sim = step.SimulationStepOptimization(arr_trj, G, obj)
-sim.set_flags_stop_simulation(flags)
-sim.change_config_system(config_sys)
-sim_output = sim.simulate_system(times_step, True)
+    flags = [FlagMaxTime(max_time)]
+
+    times_step = 1e-3
+
+    grab_obj_mat = chrono.ChMaterialSurfaceNSC()
+    grab_obj_mat.SetFriction(0.5)
+    grab_obj_mat.SetDampingF(0.1)
+    obj = chrono.ChBodyEasyBox(0.2, 0.2, 0.6, 1000, True, True, grab_obj_mat)
+    obj.SetCollide(True)
+    obj.SetPos(chrono.ChVectorD(0, 1.2, 0))
+
+    sim = step.SimulationStepOptimization(arr_trj, G, obj)
+    sim.set_flags_stop_simulation(flags)
+    sim.change_config_system(config_sys)
+    sim_output = sim.simulate_system(times_step, True)
