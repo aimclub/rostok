054aa9ebf9a7d33ed51c980d60177d6c06d1f6c5
MR-KNV <nasonov_kv@mail.ru>
2022-11-14 15:35:25 +0300

    Add nodes_division utils into main

--- app/reward_grab_mechanism.py
+++ app/reward_grab_mechanism.py
@@ -1,5 +1,6 @@
 from time import sleep,time
 import context
+import sys
 
 from engine.node  import BlockWrapper, Node, Rule, GraphGrammar, ROOT
 from engine.node_render import *
@@ -20,6 +21,8 @@ import numpy as np
 import utils.simulation_step as simulation_step
 import engine.robot as robot
 import engine.control as control
+from numpy import arange
+from utils.blocks_utils import NodeFeatures
 
 
 def is_body(node: Node): return node in list_B
@@ -272,6 +275,46 @@ node_list_plain = list(map(G.get_node_by_id,
 config_sys = {"Set_G_acc":chrono.ChVectorD(0,-10,0)}
 
 
+def create_const_traj(torque_value, stop_time: float, time_step: float):
+    timeseries_traj = []
+    timeseries = list(arange(0, stop_time, time_step))
+    traj = [torque_value for _ in timeseries]
+    timeseries_traj.append(timeseries)
+    timeseries_traj.append(traj)
+    return timeseries_traj
+
+
+def create_torque_traj_from_x(joint_dfs, x: list[float], stop_time: float, time_step: float):
+    x_iter = iter(x)
+    torque_traj = []
+    for branch in joint_dfs:
+        control_one_branch = []
+        for block in branch:
+            one_torque = next(x_iter)
+            control_one_branch.append(create_const_traj(
+                one_torque, stop_time, time_step))
+        torque_traj.append(np.array(control_one_branch))
+
+    return torque_traj
+
+
+
+dfs_patrion_ids = G.graph_partition_dfs()
+def get_node(node_id): return G.get_node_by_id(node_id)
+
+dfs_patrion_node = [[get_node(node_id) for node_id in branch]
+                    for branch in dfs_patrion_ids]
+dfs_j = []
+number_trq = 0
+for branch in dfs_patrion_node:
+    joint_branch = list(filter(NodeFeatures.is_joint, branch))
+    len_joints = len(joint_branch)
+    number_trq += len_joints
+    if len_joints != 0:
+        dfs_j.append(joint_branch)
+
+const_torque_koef = [random.random() for _ in range(number_trq)]
+arr_trj = create_torque_traj_from_x(dfs_j, const_torque_koef, 10, 0.1)
 
 
 time_model = time()
@@ -283,12 +326,21 @@ flags = [FlagSlipout(time_to_contact,time_without_contact),
 
 times_step = 1e-3
 
-if __name__ == '__main__':
-    sim = simulation_step.SimulationStepOptimization(G, obj)
-    sim.set_flags_stop_simulation(flags)
-    sim.change_config_system(config_sys)
-    sim_output = sim.simulate_system(times_step, True)
+WEIGHTS = [5, 1, 1, 5]
+GAIT_PERIOD = 2.5
 
-    reward = criterion_calc(sim, sim_output)
-    print(reward)
-    print(None)
+# if __name__ == '__main__':
+sim = simulation_step.SimulationStepOptimization(arr_trj, G, obj)
+J_NODES_NEW = nodes_division(sim.grab_robot, list_J)
+B_NODES_NEW = nodes_division(sim.grab_robot, list_B)
+RB_NODES_NEW = sort_left_right(sim.grab_robot, list_RM, list_B)
+LB_NODES_NEW = sort_left_right(sim.grab_robot, list_LM, list_B)
+RJ_NODES_NEW = sort_left_right(sim.grab_robot, list_RM, list_J)
+LJ_NODES_NEW = sort_left_right(sim.grab_robot, list_LM, list_J)
+sim.set_flags_stop_simulation(flags)
+sim.change_config_system(config_sys)
+sim_output = sim.simulate_system(times_step, True)
+
+reward = criterion_calc(sim_output, B_NODES_NEW, J_NODES_NEW, LB_NODES_NEW, RB_NODES_NEW, WEIGHTS, GAIT_PERIOD)
+print(reward)
+print(None)
