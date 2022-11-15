8a5f0d63cc2d01bf0405325f2b53b10b9d005451
ZharkovKirill <labradudel13@gmail.com>
2022-11-15 15:26:21 +0300

    It's works

--- app/app_vocabulary.py
+++ app/app_vocabulary.py
@@ -3,16 +3,13 @@ import context
 from engine import rule_vocabulary
 from engine import node_vocabulary
 from engine.node import ROOT, GraphGrammar, BlockWrapper
-import engine.robot as robot
-import engine.control as ctrl
-from engine.node_render import *
-from utils.blocks_utils import make_collide, CollisionGroup   
+from engine.node_render import ChronoBody, ChronoTransform, ChronoRevolveJoint
+from utils.transform_srtucture import FrameTransform
 
 # imports from standard libs
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-from time import sleep
 
 # chrono imports
 import pychrono as chrono
@@ -22,37 +19,37 @@ from pychrono import Q_ROTATE_Z_TO_Y, Q_ROTATE_Z_TO_X, \
     Q_ROTATE_X_TO_Y, Q_ROTATE_X_TO_Z
 
 
-def plot_graph(graph:GraphGrammar):
+def plot_graph(graph: GraphGrammar):
     plt.figure()
     nx.draw_networkx(graph, pos=nx.kamada_kawai_layout(graph, dim=2), node_size=800,
-                    labels={n: graph.nodes[n]["Node"].label for n in graph})
+                     labels={n: graph.nodes[n]["Node"].label for n in graph})
     plt.show()
 
-# Define materials
+
 # Define block types
 mat = chrono.ChMaterialSurfaceNSC()
 mat.SetFriction(0.5)
 mat.SetDampingF(0.1)
 
 # Bodies
-link1 = BlockWrapper(ChronoBody, length=0.3)
-link2 = BlockWrapper(ChronoBody, length=0.2)
+link1 = BlockWrapper(ChronoBody, length=0.6)
+link2 = BlockWrapper(ChronoBody, length=0.4)
 
-flat1 = BlockWrapper(ChronoBody, width=0.4, length=0.1)
-flat2 = BlockWrapper(ChronoBody, width=0.7, length=0.1)
+flat1 = BlockWrapper(ChronoBody, width=0.8, length=0.2)
+flat2 = BlockWrapper(ChronoBody, width=0.4, length=0.2)
 
-u1 = BlockWrapper(ChronoBody, width=0.1, length=0.1, material=mat)
+u1 = BlockWrapper(ChronoBody, width=0.2, length=0.2)
 
 # Transforms
 RZX = ChCoordsysD(ChVectorD(0, 0, 0), Q_ROTATE_Z_TO_X)
 RZY = ChCoordsysD(ChVectorD(0, 0, 0), Q_ROTATE_Z_TO_Y)
 RXY = ChCoordsysD(ChVectorD(0, 0, 0), Q_ROTATE_X_TO_Y)
 
-MOVE_ZX_PLUS = ChCoordsysD(ChVectorD(0.3, 0, 0.3), ChQuaternionD(1, 0, 0, 0))
-MOVE_ZX_MINUS = ChCoordsysD(ChVectorD(-0.3, 0, -0.3), ChQuaternionD(1, 0, 0, 0))
+MOVE_ZX_PLUS = FrameTransform([0.3, 0, 0.3], [1, 0, 0, 0])
+MOVE_ZX_MINUS = FrameTransform([-0.3, 0, -0.3], [1, 0, 0, 0])
 
-MOVE_X_PLUS = ChCoordsysD(ChVectorD(0.3, 0, 0), ChQuaternionD(1, 0, 0, 0))
-MOVE_Z_PLUS_X_MINUS = ChCoordsysD(ChVectorD(-0.3, 0, 0.3), ChQuaternionD(1, 0, 0, 0))
+MOVE_X_PLUS = FrameTransform([0.3, 0, 0.], [1, 0, 0, 0])
+MOVE_Z_PLUS_X_MINUS = FrameTransform([-0.3, 0, 0.3], [1, 0, 0, 0])
 
 transform_rzx = BlockWrapper(ChronoTransform, RZX)
 transform_rzy = BlockWrapper(ChronoTransform, RZY)
@@ -64,14 +61,16 @@ transform_mz_plus_x_minus = BlockWrapper(ChronoTransform, MOVE_Z_PLUS_X_MINUS)
 
 type_of_input = ChronoRevolveJoint.InputType.Torque
 # Joints
-revolve1 = BlockWrapper(ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z,  type_of_input)
+revolve1 = BlockWrapper(
+    ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z,  type_of_input)
 
 
 # %%
-type_of_input = ChronoRevolveJoint.InputType.Position
+type_of_input = ChronoRevolveJoint.InputType.Torque
 
 # Joints
-revolve1 = BlockWrapper(ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z,  type_of_input)
+revolve1 = BlockWrapper(
+    ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z,  type_of_input)
 
 app_node_vocab = node_vocabulary.NodeVocabulary()
 app_node_vocab.add_node(ROOT)
@@ -82,83 +81,96 @@ app_node_vocab.create_node("M")
 app_node_vocab.create_node("EF")
 app_node_vocab.create_node("EM")
 
-app_node_vocab.create_node(label="J1", is_terminal=True, block_wrapper=revolve1)
+app_node_vocab.create_node(
+    label="J1", is_terminal=True, block_wrapper=revolve1)
 app_node_vocab.create_node(label="L1", is_terminal=True, block_wrapper=link1)
 app_node_vocab.create_node(label="L2", is_terminal=True, block_wrapper=link2)
 app_node_vocab.create_node(label="F1", is_terminal=True, block_wrapper=flat1)
 app_node_vocab.create_node(label="F2", is_terminal=True, block_wrapper=flat2)
 app_node_vocab.create_node(label="U1", is_terminal=True, block_wrapper=u1)
-app_node_vocab.create_node(label="T1", is_terminal=True, block_wrapper=transform_mx_plus)
-app_node_vocab.create_node(label="T2", is_terminal=True, block_wrapper=transform_mz_plus_x_minus)
-app_node_vocab.create_node(label="T3", is_terminal=True, block_wrapper=transform_mzx_plus)
-app_node_vocab.create_node(label="T4", is_terminal=True, block_wrapper=transform_mzx_minus)
+app_node_vocab.create_node(
+    label="T1", is_terminal=True, block_wrapper=transform_mx_plus)
+app_node_vocab.create_node(
+    label="T2", is_terminal=True, block_wrapper=transform_mz_plus_x_minus)
+app_node_vocab.create_node(
+    label="T3", is_terminal=True, block_wrapper=transform_mzx_plus)
+app_node_vocab.create_node(
+    label="T4", is_terminal=True, block_wrapper=transform_mzx_minus)
 
 
 app_rule_vocab = rule_vocabulary.RuleVocabulary(app_node_vocab)
 
-app_rule_vocab.create_rule("FlatCreate", ["ROOT"], ["F"], 0 , 0)
-app_rule_vocab.create_rule("Mount", ["F"], ["F", "M", "EM"], 0 , 0, [(0,1), (1,2)])
-app_rule_vocab.create_rule("MountAdd", ["M"], ["M", "EM"], 0 , 1, [(0,1)])
-app_rule_vocab.create_rule("FingerUpper", ["EM"], ["J", "L", "EM"], 0 , 2, [(0,1), (1,2)])
+app_rule_vocab.create_rule("FlatCreate", ["ROOT"], ["F"], 0, 0)
+app_rule_vocab.create_rule(
+    "Mount", ["F"], ["F", "M", "EM"], 0, 0, [(0, 1), (1, 2)])
+app_rule_vocab.create_rule("MountAdd", ["M"], ["M", "EM"], 0, 1, [(0, 1)])
+app_rule_vocab.create_rule("FingerUpper", ["EM"], [
+                           "J", "L", "EM"], 0, 2, [(0, 1), (1, 2)])
 
 
-app_rule_vocab.create_rule("TerminalFlat1", ["F"], ["F1"], 0 ,0)
-app_rule_vocab.create_rule("TerminalFlat2", ["F"], ["F2"], 0 ,0)
+app_rule_vocab.create_rule("TerminalFlat1", ["F"], ["F1"], 0, 0)
+app_rule_vocab.create_rule("TerminalFlat2", ["F"], ["F2"], 0, 0)
 
-app_rule_vocab.create_rule("TerminalL1", ["L"], ["L1"], 0 ,0)
-app_rule_vocab.create_rule("TerminalL2", ["L"], ["L2"], 0 ,0)
+app_rule_vocab.create_rule("TerminalL1", ["L"], ["L1"], 0, 0)
+app_rule_vocab.create_rule("TerminalL2", ["L"], ["L2"], 0, 0)
 
-app_rule_vocab.create_rule("TerminalTransformRX", ["M"], ["T1"], 0 ,0)
-app_rule_vocab.create_rule("TerminalTransformLZ", ["M"], ["T2"], 0 ,0)
-app_rule_vocab.create_rule("TerminalTransformR", ["M"], ["T3"], 0 ,0)
-app_rule_vocab.create_rule("TerminalTransformL", ["M"], ["T4"], 0 ,0)
-app_rule_vocab.create_rule("TerminalEndLimb", ["EM"], ["U1"], 0 ,0)
-app_rule_vocab.create_rule("TerminalJoint", ["J"], ["J1"], 0 ,0)
+app_rule_vocab.create_rule("TerminalTransformRX", ["M"], ["T1"], 0, 0)
+app_rule_vocab.create_rule("TerminalTransformLZ", ["M"], ["T2"], 0, 0)
+app_rule_vocab.create_rule("TerminalTransformR", ["M"], ["T3"], 0, 0)
+app_rule_vocab.create_rule("TerminalTransformL", ["M"], ["T4"], 0, 0)
+app_rule_vocab.create_rule("TerminalEndLimb", ["EM"], ["U1"], 0, 0)
+app_rule_vocab.create_rule("TerminalJoint", ["J"], ["J1"], 0, 0)
 
-app_rule_vocab.check_rules()
+ 
 
+# Required for criteria calc
 list_J = list(map(app_node_vocab.get_node, ["J1"]))
 list_RM = list(map(app_node_vocab.get_node, ["T1", "T3"]))
 list_LM = list(map(app_node_vocab.get_node, ["T2", "T4"]))
 list_B = list(map(app_node_vocab.get_node, ["L1", "L2", "F1", "F2", "U1"]))
-list_Palm = list(map(app_node_vocab.get_node, ["F1"]))
+node_features = [list_B, list_J, list_LM, list_RM]
 
 
-def get_random_graph(n_iter: int, rule_vocab: rule_vocabulary.RuleVocabulary = app_rule_vocab, use_nonterminal_only: bool=True):
+def get_random_graph(n_iter: int, rule_vocab: rule_vocabulary.RuleVocabulary = app_rule_vocab, use_nonterminal_only: bool = True):
     G = GraphGrammar()
     for _ in range(n_iter//2+1):
         rules = rule_vocab.get_list_of_applicable_nonterminal_rules(G)
-        if len(rules)>0:
+        if len(rules) > 0:
             rule = rule_vocab.get_rule(rules[np.random.choice(len(rules))])
             G.apply_rule(rule)
-        else: break
-    
+        else:
+            break
+
     for _ in range(n_iter-(n_iter//2+1)):
         if use_nonterminal_only:
             rules = rule_vocab.get_list_of_applicable_nonterminal_rules(G)
-        else: rules = rule_vocab.get_list_of_applicable_rules(G)
-        if len(rules)>0:
+        else:
+            rules = rule_vocab.get_list_of_applicable_rules(G)
+        if len(rules) > 0:
             rule = rule_vocab.get_rule(rules[np.random.choice(len(rules))])
             G.apply_rule(rule)
-        else: break
+        else:
+            break
     rule_vocab.make_graph_terminal(G)
     return G
+
 
 def get_three_finger_graph():
     G = GraphGrammar()
     rule_action_non_terminal_three_finger = np.asarray(["FlatCreate", "Mount", "Mount", "Mount",
-                                       "FingerUpper", "FingerUpper", "FingerUpper",
-                                       "FingerUpper",  "FingerUpper", "FingerUpper"])
+                                                        "FingerUpper", "FingerUpper", "FingerUpper",
+                                                        "FingerUpper",  "FingerUpper", "FingerUpper"])
     rule_action_terminal_three_finger = np.asarray(["TerminalFlat1",
-                                    "TerminalL1", "TerminalL1", "TerminalL1",
-                                    "TerminalL2", "TerminalL2", "TerminalL2",
-                                    "TerminalTransformL", "TerminalTransformLZ",
-                                    "TerminalTransformRX",
-                                    "TerminalEndLimb", "TerminalEndLimb",
-                                    "TerminalEndLimb",
-                                    "TerminalJoint", "TerminalJoint", "TerminalJoint",
-                                    "TerminalJoint", "TerminalJoint", "TerminalJoint"])
-    rule_action_three_finger = np.r_[rule_action_non_terminal_three_finger, rule_action_terminal_three_finger]
+                                                    "TerminalL1", "TerminalL1", "TerminalL1",
+                                                    "TerminalL2", "TerminalL2", "TerminalL2",
+                                                    "TerminalTransformL", "TerminalTransformLZ",
+                                                    "TerminalTransformRX",
+                                                    "TerminalEndLimb", "TerminalEndLimb",
+                                                    "TerminalEndLimb",
+                                                    "TerminalJoint", "TerminalJoint", "TerminalJoint",
+                                                    "TerminalJoint", "TerminalJoint", "TerminalJoint"])
+    rule_action_three_finger = np.r_[
+        rule_action_non_terminal_three_finger, rule_action_terminal_three_finger]
     rules = list(map(app_rule_vocab.get_rule, rule_action_three_finger))
     for i in list(rules):
         G.apply_rule(i)
--- app/control_optimisation.py
+++ app/control_optimisation.py
@@ -6,12 +6,41 @@ import pychrono as chrono
 from numpy import arange
 import numpy as np
 from utils.blocks_utils import NodeFeatures
-from utils.simulation_step import SimulationStepOptimization
+from utils.simulation_step import SimulationStepOptimization,  SimulationDataBlock
 from engine.node import Node
 from typing import Union
 import utils.criterion_calc as criterion
+from utils.flags_simualtions import FlagSlipout, FlagNotContact, FlagMaxTime
+from scipy.optimize import shgo
+import random
 
-def create_dfs_joint(graph: GraphGrammar) -> Union[list[list[Node]], int]:
+
+def create_multidimensional_bounds(graph: GraphGrammar, one_d_bound: tuple[float, float]):
+    num = num_joints(graph)
+    multidimensional_bounds = []
+    for i in range(num):
+        multidimensional_bounds.append(one_d_bound)
+
+    return multidimensional_bounds
+
+
+def num_joints(graph: GraphGrammar):
+    line_order = graph.get_ids_in_dfs_order()
+    list_nodes = list(map(graph.get_node_by_id, line_order))
+    return sum(map(NodeFeatures.is_joint, list_nodes))
+
+
+def get_object_to_grasp():
+    grab_obj_mat = chrono.ChMaterialSurfaceNSC()
+    grab_obj_mat.SetFriction(0.5)
+    grab_obj_mat.SetDampingF(0.1)
+    obj = chrono.ChBodyEasyBox(0.2, 0.2, 0.6, 1000, True, True, grab_obj_mat)
+    obj.SetCollide(True)
+    obj.SetPos(chrono.ChVectorD(0, 1.2, 0))
+    return obj
+
+
+def create_dfs_joint(graph: GraphGrammar) -> list[list[Node]]:
     dfs_patrion_ids = graph.graph_partition_dfs()
     def get_node(node_id): return graph.get_node_by_id(node_id)
 
@@ -28,7 +57,7 @@ def create_dfs_joint(graph: GraphGrammar) -> Union[list[list[Node]], int]:
         if len_joints != 0:
             dfs_j.append(joint_branch)
 
-    return [dfs_j, number_trq]
+    return dfs_j
 
 
 def create_const_traj(torque_value, stop_time: float, time_step: float):
@@ -38,7 +67,6 @@ def create_const_traj(torque_value, stop_time: float, time_step: float):
     timeseries_traj.append(timeseries)
     timeseries_traj.append(traj)
     return timeseries_traj
-
 
 
 def create_torque_traj_from_x(joint_dfs, x: list[float], stop_time: float, time_step: float):
@@ -53,27 +81,66 @@ def create_torque_traj_from_x(joint_dfs, x: list[float], stop_time: float, time_
         torque_traj.append(np.array(control_one_branch))
 
     return torque_traj
- 
 
-def create_reward_fun(generated_graph: GraphGrammar, config_sys, stop_time, time_step_traj, flags, object_to_grab, list_B):
-     
-    
+
+def grab_crtitrion(sim_output: dict[int, SimulationDataBlock], grab_robot, node_feature: list[list[Node]]):
+    gait = 2.5
+    weight = [1, 1, 1, 1]
+    j_nodes = criterion.nodes_division(
+        grab_robot, node_feature[1])
+    b_nodes = criterion.nodes_division(
+        grab_robot, node_feature[0])
+    rb_nodes = criterion.sort_left_right(
+        grab_robot, node_feature[3], node_feature[0])
+    lb_nodes = criterion.sort_left_right(
+        grab_robot, node_feature[2], node_feature[0])
+
+    return criterion.criterion_calc(sim_output, b_nodes, j_nodes, rb_nodes, lb_nodes, weight, gait)
+
+
+def grab_crtitrion_with_context(sim_output: dict[int, SimulationDataBlock], grab_robot):
+    return grab_crtitrion(sim_output, grab_robot, app_vocabulary.node_features)
+
+
+def create_reward_fun(generated_graph: GraphGrammar, config_sys, stop_time, time_step_traj, flags, object_to_grab, criterion_callback):
+
     init_pos = chrono.ChCoordsysD(object_to_grab.GetCoord())
-    
+    dfs_j = create_dfs_joint(generated_graph)
+
     def reward(x):
-        
         # Init object state
+        is_vis = random.random() > 0.95
         object_to_grab.SetNoSpeedNoAcceleration()
         object_to_grab.SetCoord(init_pos)
-       
-        arr_traj = x
+        arr_traj = create_torque_traj_from_x(
+            dfs_j, x, stop_time, time_step_traj)
         sim = SimulationStepOptimization(
             arr_traj, generated_graph, object_to_grab)
         sim.set_flags_stop_simulation(flags)
         sim.change_config_system(config_sys)
-        sim_output = sim.simulate_system(0.01)
-        
-
-        return -rew
+        sim_output = sim.simulate_system(0.001, False)
+        rew = criterion_callback(sim_output, sim.grab_robot)
+        print(rew)
+        return rew
 
     return reward
+
+
+mechanism_graph = app_vocabulary.get_three_finger_graph()
+
+
+flags = [FlagMaxTime(2)]
+
+times_step = 1e-3
+bound = (-5, 5)
+
+config_sys = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
+obj = get_object_to_grasp()
+
+rewa = create_reward_fun(mechanism_graph, config_sys,
+                         2, 0.01, flags, obj, grab_crtitrion_with_context)
+multi_bound = create_multidimensional_bounds(mechanism_graph, bound)
+result = shgo(rewa, multi_bound, n=10, iters=2)
+
+print(result.x)
+print(result.fun)
