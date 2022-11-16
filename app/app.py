01c02657c7008fd180f3357b24bdb58ffb3792d0
ZharkovKirill <labradudel13@gmail.com>
2022-11-16 20:26:49 +0300

    [app] Tune optimize control parametrs

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ from engine import node_vocabulary, rule_vocabulary
 from engine.node import ROOT, BlockWrapper, GraphGrammar
 from engine.node_render import ChronoBody, ChronoRevolveJoint, ChronoTransform
 from utils.control_optimizer import ConfigRewardFunction, ControlOptimizer
-from utils.flags_simualtions import FlagMaxTime
+from utils.flags_simualtions import FlagMaxTime, FlagSlipout, FlagNotContact
 from utils.transform_srtucture import FrameTransform
 import rule_extention
 import app_vocabulary
@@ -31,7 +31,7 @@ def plot_graph(graph: GraphGrammar):
 
 # %% Create extension rule vocabulary
 
-rule_vocabul = rule_extention.init_extension_rules()
+rule_vocabul, node_features = rule_extention.init_extension_rules()
 
 # %% Create condig optimizing control
 
@@ -39,16 +39,16 @@ GAIT = 2.5
 WEIGHT = [1, 1, 1, 1]
 
 cfg = ConfigRewardFunction()
-cfg.bound = (-5, 5)
-cfg.iters = 2
+cfg.bound = (0, 10)
+cfg.iters = 10
 cfg.sim_config = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
-cfg.time_step = 0.001
-cfg.time_sim = 2
-cfg.flags = [FlagMaxTime(2)]
+cfg.time_step = 0.0015
+cfg.time_sim = 3
+cfg.flags = [FlagMaxTime(3)]
 
 """Wraps function call"""
 
-criterion_callback = create_grab_criterion_fun(app_vocabulary.node_features, GAIT, WEIGHT)
+criterion_callback = create_grab_criterion_fun(node_features, GAIT, WEIGHT)
 traj_generator_fun = create_traj_fun(cfg.time_sim, cfg.time_step)
 
 cfg.criterion_callback = criterion_callback
@@ -70,7 +70,7 @@ searcher = mcts.mcts(iterationLimit=iteration_limit)
 finish = False
 
 G = GraphGrammar()
-max_numbers_rules = 5
+max_numbers_rules = 7
 # Create graph envirenments for algorithm (not gym)
 graph_env = env.GraphVocabularyEnvironment(G, rule_vocabul, max_numbers_rules)
 
@@ -85,12 +85,8 @@ while not finish:
     print(f"number iteration: {iter}, counter actions: {graph_env.counter_action}, reward: {graph_env.reward}")
     
 # %%
-
-plt.figure()
-nx.draw_networkx(final_graph, pos=nx.kamada_kawai_layout(final_graph, dim=2), node_size=800,
-                 labels={n: final_graph.nodes[n]["Node"].label for n in final_graph})
-
-plt.show()
+ 
 
 func_reward = control_optimizer.create_reward_function(final_graph)
-func_reward(opt_trajectory, True)
+res = func_reward(opt_trajectory, True)
+print(res)
