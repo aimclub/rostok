9076e0e4405a7fc36232836c52f376561d96430f
ZharkovKirill <labradudel13@gmail.com>
2022-11-16 15:29:06 +0300

    [control_optimisation] Passed hyper parametrs

--- app/control_optimisation.py
+++ app/control_optimisation.py
@@ -21,9 +21,7 @@ def get_object_to_grasp():
     return obj
 
 
-def grab_crtitrion(sim_output: dict[int, SimOut], grab_robot, node_feature: list[list[Node]]):
-    gait = 2.5
-    weight = [1, 1, 1, 1]
+def grab_crtitrion(sim_output: dict[int, SimOut], grab_robot, node_feature: list[list[Node]], gait, weight):
     j_nodes = criterion.nodes_division(
         grab_robot, node_feature[1])
     b_nodes = criterion.nodes_division(
@@ -36,9 +34,9 @@ def grab_crtitrion(sim_output: dict[int, SimOut], grab_robot, node_feature: list
     return criterion.criterion_calc(sim_output, b_nodes, j_nodes, rb_nodes, lb_nodes, weight, gait)
 
 
-def create_grab_criterion_fun(node_features):
+def create_grab_criterion_fun(node_features, gait, weight):
     def fun(sim_output, grab_robot):
-        return grab_crtitrion(sim_output, grab_robot, node_features)
+        return grab_crtitrion(sim_output, grab_robot, node_features, gait, weight)
 
     return fun
 
@@ -49,25 +47,28 @@ def create_traj_fun(stop_time: float, time_step: float):
 
     return fun
 
+if __name__ == '__main__':
+    GAIT = 2.5
+    WEIGHT = [1, 1, 1, 1]
 
-cfg = ConfigRewardFunction()
-cfg.bound = (-5, 5)
-cfg.iters = 2
-cfg.sim_config = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
-cfg.time_step = 0.001
-cfg.time_sim = 2
-cfg.flags = [FlagMaxTime(2)]
+    cfg = ConfigRewardFunction()
+    cfg.bound = (-5, 5)
+    cfg.iters = 2
+    cfg.sim_config = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
+    cfg.time_step = 0.001
+    cfg.time_sim = 2
+    cfg.flags = [FlagMaxTime(2)]
 
-"""Wraps function call"""
+    """Wraps function call"""
 
-criterion_callback = create_grab_criterion_fun(app_vocabulary.node_features)
-traj_generator_fun = create_traj_fun(cfg.time_sim, cfg.time_step)
+    criterion_callback = create_grab_criterion_fun(app_vocabulary.node_features, GAIT, WEIGHT)
+    traj_generator_fun = create_traj_fun(cfg.time_sim, cfg.time_step)
 
-cfg.criterion_callback = criterion_callback
-cfg.get_rgab_object_callback = get_object_to_grasp
-cfg.params_to_timesiries_callback = traj_generator_fun
+    cfg.criterion_callback = criterion_callback
+    cfg.get_rgab_object_callback = get_object_to_grasp
+    cfg.params_to_timesiries_callback = traj_generator_fun
 
-control_optimizer = ControlOptimizer(cfg)
-graph = app_vocabulary.get_three_finger_graph()
-res = control_optimizer.start_optimisation(graph)
-print(res)
+    control_optimizer = ControlOptimizer(cfg)
+    graph = app_vocabulary.get_three_finger_graph()
+    res = control_optimizer.start_optimisation(graph)
+    print(res)
