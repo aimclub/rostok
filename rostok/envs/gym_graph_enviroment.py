c136991f72238593f03decc73b262dddeb7beac8
Huowl <kraken.mix@gmail.com>
2022-11-29 12:40:02 +0300

    [gym] Add render and mask possible action

--- rostok/envs/gym_graph_enviroment.py
+++ rostok/envs/gym_graph_enviroment.py
@@ -11,7 +11,8 @@ from rostok.graph_grammar.rule_vocabulary import RuleVocabulary
 
 
 def networkx_2_gym_digraph(nx_graph: GraphGrammar, table_nodes_id: dict[str, int]):
-    list_id_nodes = np.asarray(list(table_nodes_id[node[1]["Node"].label] for node in nx_graph.nodes.items()))
+    list_id_nodes = np.asarray(
+        list(table_nodes_id[node[1]["Node"].label] for node in nx_graph.nodes.items()))
 
     table_id_index_nodes = dict((id[1], id[0]) for id in enumerate(nx_graph.nodes.keys()))
     nx_edges = list(nx_graph.edges)
@@ -27,7 +28,7 @@ def networkx_2_gym_digraph(nx_graph: GraphGrammar, table_nodes_id: dict[str, int
 
 
 class GraphGrammarEnv(gym.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+    metadata = {"render_modes": ["grammar", "simulation", "grammar&simulation"], "render_fps": 4}
 
     def __init__(self, rule_vocabulary: RuleVocabulary, controller, render_mode=None):
 
@@ -54,7 +55,11 @@ class GraphGrammarEnv(gym.Env):
 
         self.controller = controller
 
-        assert render_mode is None or render_mode is self.metadata["render_modes"]
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        if render_mode in ("grammar", "grammar&simulation"):
+            self._grammar_render = plt.subplots()
 
     def _get_initial_obs(self):
         observation = spaces.GraphInstance([0], None, None)
@@ -65,15 +70,10 @@ class GraphGrammarEnv(gym.Env):
         return observation
 
     def _sim(self):
-        # FIXME: Change to interface controller
+        #FIXME: Change to interface controller
         result_optimizer = self.controller.start_optimisation(self.graph_grammar)
         reward = -result_optimizer[0]
         movments_trajectory = result_optimizer[1]
-
-        # TODO: Render mode
-        # if self.render_modes == "human":
-        #     func_reward = self.optimizer.create_reward_function(self.graph)
-        #     func_reward(self.movments_trajectory, True)
         return reward, movments_trajectory
 
     def _get_info(self):
@@ -81,10 +81,13 @@ class GraphGrammarEnv(gym.Env):
             "num_nodes": len(list(self.graph_grammar.nodes)),
             "num_applicable_rules": {
                 "nonterminal":
-                    self.rule_vocabulary.get_list_of_applicable_nonterminal_rules(self.graph_grammar
-                                                                                 ),
+                    len(
+                        self.rule_vocabulary.get_list_of_applicable_nonterminal_rules(
+                            self.graph_grammar)),
                 "terminal":
-                    self.rule_vocabulary.get_list_of_applicable_terminal_rules(self.graph_grammar)
+                    len(
+                        self.rule_vocabulary.get_list_of_applicable_terminal_rules(
+                            self.graph_grammar))
             },
             #"num_aplied_rules": {"nonterminal": self.rule_vocabulary}
         }
@@ -98,34 +101,45 @@ class GraphGrammarEnv(gym.Env):
         info = self._get_info()
         return observation, info
 
-    # TODO: Render mode
-    def render(self):
-        if self.render_mode == "human":
-            plt.figure()
+    def render(self, terminated, movment_trajectory=None):
+        if self.render_mode in ("grammar", "grammar&simulation"):
+            self._grammar_render[0].show()
+            self._grammar_render[1].clear()
             nx.draw_networkx(
                 self.graph_grammar,
-                pos=nx.kamada_kawai_layout(self.graph, dim=2),
+                pos=nx.kamada_kawai_layout(self.graph_grammar, dim=2),
                 node_size=800,
                 labels={n: self.graph_grammar.nodes[n]["Node"].label for n in self.graph_grammar})
-            plt.show()
-            sleep(5)
-            plt.close()
+            plt.pause(0.5)
+        if self.render_mode in ("simulation", "grammar&simulation") and terminated is True:
+            func_reward = self.controller.create_reward_function(self.graph_grammar)
+            if movment_trajectory is None:
+                raise Exception("To set movment trajectory ror simulation render")
+            func_reward(movment_trajectory, True)
 
     def step(self, action):
 
-        str_rule = self.table_rules[action]
-        self.graph_grammar.apply_rule(self.rule_vocabulary.get_rule(str_rule))
+        applicable_rules = self.rule_vocabulary.get_list_of_applicable_rules(self.graph_grammar)
+        mask_applicable_rules = map(lambda x: x in applicable_rules, self.table_rules.values())
+        mask_applicable_rules = np.asarray(list(mask_applicable_rules))
+        actions = np.asarray(list(self.table_rules.keys()))
+        if action in actions[mask_applicable_rules]:
+            str_rule = self.table_rules[action]
+            self.graph_grammar.apply_rule(self.rule_vocabulary.get_rule(str_rule))
 
-        terminated = (len(
-            self.rule_vocabulary.get_list_of_applicable_nonterminal_rules(self.graph_grammar)) == 0)
+        trajectory = None
 
-        self.render()
+        info = self._get_info()
+        terminated = (info["num_applicable_rules"]["nonterminal"] +
+                      info["num_applicable_rules"]["terminal"] == 0)
+
         if terminated:
-            reward, _ = self._sim()
+            reward, trajectory = self._sim()
         else:
             reward = 0.
 
+        self.render(terminated, trajectory)
+
         observation = self._get_obs()
-        info = self._get_info()
 
         return observation, reward, terminated, False, info
