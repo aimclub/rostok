33096b917778453f36df1c58c0082cc59acd8227
Huowl <kraken.mix@gmail.com>
2022-11-30 12:43:41 +0300

    Add examples working gym

--- examples/examples_grammar_gym.py
+++ examples/examples_grammar_gym.py
@@ -1,17 +1,13 @@
-from control_optimisation import create_grab_criterion_fun, create_traj_fun, get_object_to_grasp
-from rostok.graph_grammar.node import GraphGrammar
-from rostok.trajectory_optimizer.control_optimizer import ConfigRewardFunction, ControlOptimizer
-from rostok.criterion.flags_simualtions import FlagMaxTime, FlagSlipout, FlagNotContact
-import rule_extention
-import mcts
-import rostok.envs.gym_graph_enviroment as env
-import matplotlib.pyplot as plt
-
-# imports from standard libs
-import networkx as nx
-
 # chrono imports
 import pychrono as chrono
+
+from control_optimisation import create_grab_criterion_fun, create_traj_fun, get_object_to_grasp
+import rule_extention
+
+# imports from standard libs
+import rostok.gym_rostok.envs as env
+from rostok.trajectory_optimizer.control_optimizer import ConfigRewardFunction, ControlOptimizer
+from rostok.criterion.flags_simualtions import FlagMaxTime, FlagSlipout, FlagNotContact
 
 rule_vocabul, node_features = rule_extention.init_extension_rules()
 
@@ -22,7 +18,7 @@ WEIGHT = [1, 1, 1, 1]
 
 cfg = ConfigRewardFunction()
 cfg.bound = (0, 10)
-cfg.iters = 10
+cfg.iters = 1
 cfg.sim_config = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
 cfg.time_step = 0.0015
 cfg.time_sim = 3
@@ -38,13 +34,22 @@ cfg.params_to_timesiries_callback = traj_generator_fun
 
 control_optimizer = ControlOptimizer(cfg)
 
-# Create graph envirenments for algorithm (not gym)
-graph_env = env.GraphGrammarEnv(rule_vocabul, control_optimizer)
+# Create gym for algorithm
+graph_env = env.GGrammarControlOpimizingEnv(rule_vocabul,
+                                            control_optimizer,
+                                            render_mode="grammar&simulation")
 
 # %% Run first algorithm
 action = [0, 4, 4, 5, 20, 11, 29, 29, 30, 30, 8, 9]
-for i in range(len(action)):
+for i, a in enumerate(action):
     #graph_env.action_space.sample()
-    graph_env.step(action[i])
+    graph_env.step(a)
 
+    print(i, a)
 graph_env.reset()
+action = [1, 4, 4, 4, 4, 5, 20, 11, 29, 29, 30, 30, 8, 9]
+for i, a in enumerate(action):
+    #graph_env.action_space.sample()
+    graph_env.step(a)
+
+    print(i, a)
