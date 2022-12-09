418d93523d4d9263b1900adf68e1a50c2f2322d2
ZharkovKirill <labradudel13@gmail.com>
2022-12-09 11:34:15 +0300

    [tests] Add test for simstep and control

--- tests/test_simstep.py
+++ tests/test_simstep.py
@@ -1,96 +1,46 @@
+import random
+
+import numpy as np
+import pychrono as chrono
+from test_ruleset import (get_terminal_graph_ladoshaka,
+                          get_terminal_graph_three_finger,
+                          get_terminal_graph_two_finger)
+
 import app.app_vocabulary as voca
+import rostok.virtual_experiment.simulation_step as step
+from rostok.criterion.flags_simualtions import FlagMaxTime
+from rostok.trajectory_optimizer.control_optimizer import num_joints
+from rostok.trajectory_optimizer.trajectory_generator import (
+    create_dfs_joint, create_torque_traj_from_x)
 
 
-def test_passing1():
-    print("First")
-    assert (1, 2, 3) == (1, 2, 3)
-
-
-def test_stub2():
-    print("Second")
-    graph1 = voca.get_random_graph(5, voca.app_rule_vocab)
-    assert (1, 2, 3) == (1, 2, 3)
-
-
-def test3():
-    import random
-
-    import numpy as np
-    import pychrono as chrono
-    from example_ruleset import (get_terminal_graph_ladoshaka,
-                                get_terminal_graph_three_finger,
-                                get_terminal_graph_two_finger)
-    from numpy import arange
-
-    import rostok.virtual_experiment.robot as robot
-    import rostok.virtual_experiment.simulation_step as step
-    from rostok.block_builder.blocks_utils import NodeFeatures
-    from rostok.criterion.flags_simualtions import (FlagMaxTime, FlagNotContact,
-                                                    FlagSlipout)
-    from rostok.trajectory_optimizer.control_optimizer import num_joints
-    from rostok.trajectory_optimizer.trajectory_generator import create_dfs_joint
-
+def test_create_graph():
     """
-        Example generate random constant torque
-        Calculate all info about joint from graph
-        
+        Test for graph grammar and rule
     """
+    graph = voca.get_random_graph(5, voca.app_rule_vocab)
 
 
-    def create_const_traj(torque_value, stop_time: float, time_step: float):
-        timeseries_traj = []
-        timeseries = list(arange(0, stop_time, time_step))
-        traj = [torque_value for _ in timeseries]
-        timeseries_traj.append(timeseries)
-        timeseries_traj.append(traj)
-        return timeseries_traj
-
-
-    def create_torque_traj_from_x(joint_dfs, x: list[float], stop_time: float, time_step: float):
-        x_iter = iter(x)
-        torque_traj = []
-        for branch in joint_dfs:
-            control_one_branch = []
-            for block in branch:
-                one_torque = next(x_iter)
-                control_one_branch.append(create_const_traj(
-                    one_torque, stop_time, time_step))
-            torque_traj.append(np.array(control_one_branch))
-
-        return torque_traj
-
-
+def test_control_bind_and_create_sim():
+    """
+        Test for simulation class, control binder and control generator
+    """
+    
     mechs = [get_terminal_graph_three_finger,
             get_terminal_graph_ladoshaka, get_terminal_graph_two_finger]
 
     for get_graph in mechs:
         G = get_graph()
-
-        dfs_patrion_ids = G.graph_partition_dfs()
-        def get_node(node_id): return G.get_node_by_id(node_id)
-
-        dfs_patrion_node = [[get_node(node_id) for node_id in branch]
-                            for branch in dfs_patrion_ids]
         dfs_j = create_dfs_joint(G)
         number_trq = num_joints(G)
-        
-        const_torque_koef = [random.random() for _ in range(number_trq)]
-        arr_trj = create_torque_traj_from_x(dfs_j, const_torque_koef, 10, 0.1)
-
-        chrono_system = chrono.ChSystemNSC()
-        grab_robot = robot.Robot(G, chrono_system)
-
-        joints = np.array(grab_robot.get_joints)
 
         config_sys = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
-
-        time_to_contact = 2
-        time_without_contact = 0.2
-        max_time = 2
-
+        max_time = 1
         flags = [FlagMaxTime(max_time)]
-
         times_step = 1e-3
+
+        const_torque_koef = [random.random() for _ in range(number_trq)]
+        arr_trj = create_torque_traj_from_x(dfs_j, const_torque_koef, 1, 0.1)
 
         grab_obj_mat = chrono.ChMaterialSurfaceNSC()
         grab_obj_mat.SetFriction(0.5)
