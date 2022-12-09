ff55f85190f7ecbaed8644b7d1e5c44b78e57222
ZharkovKirill <labradudel13@gmail.com>
2022-12-09 14:26:09 +0300

    [test_ruleset] Update from api cascade patch

--- tests/test_ruleset.py
+++ tests/test_ruleset.py
@@ -1,13 +1,15 @@
-import pychrono as chrono
+from cmath import sqrt
+
 import networkx as nx
 import numpy as np
+import pychrono as chrono
 
-from rostok.graph_grammar.node import BlockWrapper, Node, Rule, GraphGrammar, ROOT
-from rostok.block_builder.node_render import ChronoBody, ChronoTransform, ChronoRevolveJoint
-
-from rostok.block_builder.blocks_utils import make_collide, CollisionGroup
+from rostok.block_builder.node_render import (ChronoRevolveJoint, ChronoTransform, FlatChronoBody,
+                                              LinkChronoBody, MountChronoBody)
 from rostok.block_builder.transform_srtucture import FrameTransform
-
+from rostok.graph_grammar.node import ROOT, BlockWrapper, GraphGrammar
+from rostok.graph_grammar.node_vocabulary import NodeVocabulary
+from rostok.graph_grammar.rule_vocabulary import RuleVocabulary
 
 # Define block types
 mat = chrono.ChMaterialSurfaceNSC()
@@ -15,232 +17,139 @@ mat.SetFriction(0.5)
 mat.SetDampingF(0.1)
 
 # Bodies
-link1 = BlockWrapper(ChronoBody, length=0.6)
-link2 = BlockWrapper(ChronoBody, length=0.4)
+link1 = BlockWrapper(LinkChronoBody, length=0.3)
+link2 = BlockWrapper(LinkChronoBody, length=0.2)
 
-flat1 = BlockWrapper(ChronoBody, width=0.8, length=0.2)
-flat2 = BlockWrapper(ChronoBody, width=1.4, length=0.2)
+flat1 = BlockWrapper(FlatChronoBody, width=0.4, length=0.1)
+flat2 = BlockWrapper(FlatChronoBody, width=0.7, length=0.1)
 
-u1 = BlockWrapper(ChronoBody, width=0.2, length=0.2)
+u1 = BlockWrapper(MountChronoBody, width=0.1, length=0.1)
 
 # Transforms
-MOVE_ZX_PLUS = FrameTransform([0.3,0,0.3],[1,0,0,0])
-MOVE_ZX_MINUS = FrameTransform([-0.3,0,-0.3],[1,0,0,0])
+RZX = FrameTransform([0, 0, 0], [sqrt(2), 0, sqrt(2), 0])
+RZY = FrameTransform([0, 0, 0], [sqrt(2), -sqrt(2), 0, 0])
+RXY = FrameTransform([0, 0, 0], [sqrt(2), 0, 0, sqrt(2)])
 
-MOVE_X_PLUS = FrameTransform([0.3,0,0.],[1,0,0,0])
-MOVE_Z_PLUS_X_MINUS = FrameTransform([-0.3,0,0.3],[1,0,0,0])
+MOVE_ZX_PLUS = FrameTransform([0.3, 0, 0.3], [1, 0, 0, 0])
+MOVE_ZX_MINUS = FrameTransform([-0.3, 0, -0.3], [1, 0, 0, 0])
 
+MOVE_X_PLUS = FrameTransform([0.3, 0, 0.], [1, 0, 0, 0])
+MOVE_Z_PLUS_X_MINUS = FrameTransform([-0.3, 0, 0.3], [1, 0, 0, 0])
+
+transform_rzx = BlockWrapper(ChronoTransform, RZX)
+transform_rzy = BlockWrapper(ChronoTransform, RZY)
+transform_rxy = BlockWrapper(ChronoTransform, RXY)
 transform_mzx_plus = BlockWrapper(ChronoTransform, MOVE_ZX_PLUS)
 transform_mzx_minus = BlockWrapper(ChronoTransform, MOVE_ZX_MINUS)
 transform_mx_plus = BlockWrapper(ChronoTransform, MOVE_X_PLUS)
 transform_mz_plus_x_minus = BlockWrapper(ChronoTransform, MOVE_Z_PLUS_X_MINUS)
 
+type_of_input = ChronoRevolveJoint.InputType.TORQUE
 # Joints
+revolve1 = BlockWrapper(ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z, type_of_input)
 
-type_of_input = ChronoRevolveJoint.InputType.Torque
-revolve1 = BlockWrapper(
-    ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z,  type_of_input)
+# %%
+type_of_input = ChronoRevolveJoint.InputType.POSITION
 
-# Nodes
+# Joints
+revolve1 = BlockWrapper(ChronoRevolveJoint, ChronoRevolveJoint.Axis.Z, type_of_input)
 
-J1 = Node(label="J1", is_terminal=True, block_wrapper=revolve1)
-L1 = Node(label="L1", is_terminal=True, block_wrapper=link1)
-L2 = Node(label="L2", is_terminal=True, block_wrapper=link2)
-F1 = Node(label="F1", is_terminal=True, block_wrapper=flat1)
-F2 = Node(label="F2", is_terminal=True, block_wrapper=flat2)
-U1 = Node(label="U1", is_terminal=True, block_wrapper=u1)
-T1 = Node(label="T1", is_terminal=True, block_wrapper=transform_mx_plus)
-T2 = Node(label="T2", is_terminal=True, block_wrapper=transform_mz_plus_x_minus)
-T3 = Node(label="T3", is_terminal=True, block_wrapper=transform_mzx_plus)
-T4 = Node(label="T4", is_terminal=True, block_wrapper=transform_mzx_minus)
+node_vocab = NodeVocabulary()
+node_vocab.add_node(ROOT)
+node_vocab.create_node("J")
+node_vocab.create_node("L")
+node_vocab.create_node("F")
+node_vocab.create_node("M")
+node_vocab.create_node("EF")
+node_vocab.create_node("EM")
 
+node_vocab.create_node(label="J1", is_terminal=True, block_wrapper=revolve1)
+node_vocab.create_node(label="L1", is_terminal=True, block_wrapper=link1)
+node_vocab.create_node(label="L2", is_terminal=True, block_wrapper=link2)
+node_vocab.create_node(label="F1", is_terminal=True, block_wrapper=flat1)
+node_vocab.create_node(label="F2", is_terminal=True, block_wrapper=flat2)
+node_vocab.create_node(label="U1", is_terminal=True, block_wrapper=u1)
+node_vocab.create_node(label="T1", is_terminal=True, block_wrapper=transform_mx_plus)
+node_vocab.create_node(label="T2", is_terminal=True, block_wrapper=transform_mz_plus_x_minus)
+node_vocab.create_node(label="T3", is_terminal=True, block_wrapper=transform_mzx_plus)
+node_vocab.create_node(label="T4", is_terminal=True, block_wrapper=transform_mzx_minus)
 
-J = Node("J")
-L = Node("L")
-F = Node("F")
-M = Node("M")
-EF = Node("EF")
-EM = Node("EM")
+rule_vocab = RuleVocabulary(node_vocab)
 
-J_NODES = [J, J1]
-B_NODES = [L, L1, L2, F1, F2]
-T_EXAMPLE = [T1, T2]
+rule_vocab.create_rule("FlatCreate", ["ROOT"], ["F"], 0, 0)
+rule_vocab.create_rule("Mount", ["F"], ["F", "M", "EM"], 0, 0, [(0, 1), (1, 2)])
+rule_vocab.create_rule("MountAdd", ["M"], ["M", "EM"], 0, 1, [(0, 1)])
+rule_vocab.create_rule("FingerUpper", ["EM"], ["J", "L", "EM"], 0, 2, [(0, 1), (1, 2)])
 
-# Defines rules
+rule_vocab.create_rule("TerminalFlat1", ["F"], ["F1"], 0, 0)
+rule_vocab.create_rule("TerminalFlat2", ["F"], ["F2"], 0, 0)
 
-# Non terminal
-FlatCreate = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=F)
-FlatCreate.id_node_connect_child = 0
-FlatCreate.id_node_connect_parent = 0
-FlatCreate.graph_insert = rule_graph
-FlatCreate.replaced_node = ROOT
+rule_vocab.create_rule("TerminalL1", ["L"], ["L1"], 0, 0)
+rule_vocab.create_rule("TerminalL2", ["L"], ["L2"], 0, 0)
 
-Mount = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=F)
-rule_graph.add_node(1, Node=M)
-rule_graph.add_node(2, Node=EM)
-rule_graph.add_edge(0, 1)
-rule_graph.add_edge(1, 2)
-Mount.id_node_connect_child = 0
-Mount.id_node_connect_parent = 2
-Mount.graph_insert = rule_graph
-Mount.replaced_node = F
+rule_vocab.create_rule("TerminalTransformRX", ["M"], ["T1"], 0, 0)
+rule_vocab.create_rule("TerminalTransformLZ", ["M"], ["T2"], 0, 0)
+rule_vocab.create_rule("TerminalTransformR", ["M"], ["T3"], 0, 0)
+rule_vocab.create_rule("TerminalTransformL", ["M"], ["T4"], 0, 0)
+rule_vocab.create_rule("TerminalEndLimb", ["EM"], ["U1"], 0, 0)
+rule_vocab.create_rule("TerminalJoint", ["J"], ["J1"], 0, 0)
 
-MountAdd = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=M)
-rule_graph.add_node(1, Node=EM)
-rule_graph.add_edge(0, 1)
-MountAdd.id_node_connect_child = 1
-MountAdd.id_node_connect_parent = 0
-MountAdd.graph_insert = rule_graph
-MountAdd.replaced_node = M
+rule_action_non_terminal_three_finger = np.asarray([
+    "FlatCreate", "Mount", "Mount", "Mount", "FingerUpper", "FingerUpper", "FingerUpper",
+    "FingerUpper", "FingerUpper", "FingerUpper"
+])
+rule_action_terminal_three_finger = np.asarray([
+    "TerminalFlat1", "TerminalL1", "TerminalL1", "TerminalL1", "TerminalL2", "TerminalL2",
+    "TerminalL2", "TerminalTransformL", "TerminalTransformLZ", "TerminalTransformRX",
+    "TerminalEndLimb", "TerminalEndLimb", "TerminalEndLimb", "TerminalJoint", "TerminalJoint",
+    "TerminalJoint", "TerminalJoint", "TerminalJoint", "TerminalJoint"
+])
+rule_action_three_finger = np.r_[rule_action_non_terminal_three_finger,
+                                 rule_action_terminal_three_finger]
 
-FingerUpper = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=J)
-rule_graph.add_node(1, Node=L)
-rule_graph.add_node(2, Node=EM)
-rule_graph.add_edge(0, 1)
-rule_graph.add_edge(1, 2)
-FingerUpper.id_node_connect_child = 2
-FingerUpper.id_node_connect_parent = 0
-FingerUpper.graph_insert = rule_graph
-FingerUpper.replaced_node = EM
+rule_action_non_terminal_two_finger = np.asarray([
+    "FlatCreate", "Mount", "Mount", "FingerUpper", "FingerUpper", "FingerUpper", "FingerUpper",
+    "FingerUpper"
+])
 
-# Terminal
-TerminalFlat = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=F1)
-TerminalFlat.id_node_connect_child = 0
-TerminalFlat.id_node_connect_parent = 0
-TerminalFlat.graph_insert = rule_graph
-TerminalFlat.replaced_node = F
-
-TerminalL1 = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=L1)
-TerminalL1.id_node_connect_child = 0
-TerminalL1.id_node_connect_parent = 0
-TerminalL1.graph_insert = rule_graph
-TerminalL1.replaced_node = L
-
-TerminalL2 = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=L2)
-TerminalL2.id_node_connect_child = 0
-TerminalL2.id_node_connect_parent = 0
-TerminalL2.graph_insert = rule_graph
-TerminalL2.replaced_node = L
-
-TerminalTransformRX = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=T1)
-TerminalTransformRX.id_node_connect_child = 0
-TerminalTransformRX.id_node_connect_parent = 0
-TerminalTransformRX.graph_insert = rule_graph
-TerminalTransformRX.replaced_node = M
-
-TerminalTransformLZ = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=T2)
-TerminalTransformLZ.id_node_connect_child = 0
-TerminalTransformLZ.id_node_connect_parent = 0
-TerminalTransformLZ.graph_insert = rule_graph
-TerminalTransformLZ.replaced_node = M
-
-TerminalTransformR = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=T3)
-TerminalTransformR.id_node_connect_child = 0
-TerminalTransformR.id_node_connect_parent = 0
-TerminalTransformR.graph_insert = rule_graph
-TerminalTransformR.replaced_node = M
-
-TerminalTransformL = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=T4)
-TerminalTransformL.id_node_connect_child = 0
-TerminalTransformL.id_node_connect_parent = 0
-TerminalTransformL.graph_insert = rule_graph
-TerminalTransformL.replaced_node = M
-
-TerminalEndLimb = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=U1)
-TerminalEndLimb.id_node_connect_child = 0
-TerminalEndLimb.id_node_connect_parent = 0
-TerminalEndLimb.graph_insert = rule_graph
-TerminalEndLimb.replaced_node = EM
-
-TerminalJoint = Rule()
-rule_graph = nx.DiGraph()
-rule_graph.add_node(0, Node=J1)
-TerminalJoint.id_node_connect_child = 0
-TerminalJoint.id_node_connect_parent = 0
-TerminalJoint.graph_insert = rule_graph
-TerminalJoint.replaced_node = J
-
-
-rule_action_non_terminal_three_finger = np.asarray([FlatCreate, Mount, Mount, Mount,
-                                       FingerUpper, FingerUpper, FingerUpper,
-                                       FingerUpper,  FingerUpper, FingerUpper])
-rule_action_terminal_three_finger = np.asarray([TerminalFlat,
-                                   TerminalL1, TerminalL1, TerminalL1,
-                                   TerminalL2, TerminalL2, TerminalL2,
-                                   TerminalTransformL, TerminalTransformLZ,
-                                   TerminalTransformRX,
-                                   TerminalEndLimb, TerminalEndLimb,
-                                   TerminalEndLimb,
-                                   TerminalJoint, TerminalJoint, TerminalJoint,
-                                   TerminalJoint, TerminalJoint, TerminalJoint])
-rule_action_three_finger = np.r_[rule_action_non_terminal_three_finger, rule_action_terminal_three_finger]
-
-
-
-rule_action_non_terminal_two_finger = np.asarray([FlatCreate, Mount, Mount,
-                                       FingerUpper, FingerUpper, FingerUpper, FingerUpper,  FingerUpper])
-
-rule_action_terminal_two_finger = np.asarray([TerminalFlat,
-                         TerminalL1, TerminalL1, TerminalL1, TerminalL2, TerminalL2,
-                         TerminalTransformL, TerminalTransformLZ,
-                         TerminalEndLimb, TerminalEndLimb,
-                         TerminalJoint, TerminalJoint, TerminalJoint, TerminalJoint, TerminalJoint])
+rule_action_terminal_two_finger = np.asarray([
+    "TerminalFlat1", "TerminalL1", "TerminalL1", "TerminalL1", "TerminalL2", "TerminalL2",
+    "TerminalTransformL", "TerminalTransformLZ", "TerminalEndLimb", "TerminalEndLimb",
+    "TerminalJoint", "TerminalJoint", "TerminalJoint", "TerminalJoint", "TerminalJoint"
+])
 rule_action_two_finger = np.r_[rule_action_non_terminal_two_finger, rule_action_terminal_two_finger]
 
+rule_action_non_terminal_ladoshaka = np.asarray(["FlatCreate", "Mount"])
 
-rule_action_non_terminal_ladoshaka = np.asarray([FlatCreate,Mount])
+rule_action_terminal_no_joints = np.asarray(
+    ["TerminalFlat1", "TerminalTransformL", "TerminalEndLimb"])
 
-rule_action_terminal_ladoshaka = np.asarray([TerminalFlat,TerminalTransformL,TerminalEndLimb])
-
-rule_action_ladoshaka = np.r_[rule_action_non_terminal_ladoshaka, rule_action_terminal_ladoshaka]
-
-
+rule_action_no_joints = np.r_[rule_action_non_terminal_ladoshaka, rule_action_terminal_no_joints]
 
 
 def get_terminal_graph_three_finger():
     G = GraphGrammar()
     for i in list(rule_action_three_finger):
-        G.apply_rule(i)
+        G.apply_rule(rule_vocab.get_rule(i))
     return G
 
-def get_terminal_graph_ladoshaka():
+
+def get_terminal_graph_no_joints():
     G = GraphGrammar()
-    for i in list(rule_action_ladoshaka):
-        G.apply_rule(i)
+    for i in list(rule_action_no_joints):
+        G.apply_rule(rule_vocab.get_rule(i))
     return G
+
 
 def get_terminal_graph_two_finger():
     G = GraphGrammar()
     for i in list(rule_action_two_finger):
-        G.apply_rule(i)
+        G.apply_rule(rule_vocab.get_rule(i))
     return G
+
 
 def get_nonterminal_graph_two_finger():
     G = GraphGrammar()
     for i in list(rule_action_non_terminal_two_finger):
-        G.apply_rule(i)
+        G.apply_rule(rule_vocab.get_rule(i))
     return G
--- tests/test_simstep.py
+++ tests/test_simstep.py
@@ -2,16 +2,21 @@ import random
 
 import numpy as np
 import pychrono as chrono
-from test_ruleset import (get_terminal_graph_ladoshaka,
+from test_ruleset import (get_terminal_graph_no_joints,
                           get_terminal_graph_three_finger,
                           get_terminal_graph_two_finger)
 
 import app.app_vocabulary as voca
 import rostok.virtual_experiment.simulation_step as step
+from rostok.block_builder.basic_node_block import SimpleBody
+from rostok.block_builder.node_render import (ChronoBodyEnv,
+                                              DefaultChronoMaterial,
+                                              FrameTransform)
 from rostok.criterion.flags_simualtions import FlagMaxTime
+from rostok.graph_grammar.node import BlockWrapper
 from rostok.trajectory_optimizer.control_optimizer import num_joints
-from rostok.trajectory_optimizer.trajectory_generator import (
-    create_dfs_joint, create_torque_traj_from_x)
+from rostok.trajectory_optimizer.trajectory_generator import \
+    create_torque_traj_from_x
 
 
 def test_create_graph():
@@ -25,9 +30,10 @@ def test_control_bind_and_create_sim():
     """
         Test for simulation class, control binder and control generator
     """
-    
-    mechs = [get_terminal_graph_three_finger,
-            get_terminal_graph_ladoshaka, get_terminal_graph_two_finger]
+
+    mechs = [
+        get_terminal_graph_three_finger, get_terminal_graph_no_joints, get_terminal_graph_two_finger
+    ]
 
     for get_graph in mechs:
         G = get_graph()
@@ -41,12 +47,13 @@ def test_control_bind_and_create_sim():
         const_torque_koef = [random.random() for _ in range(number_trq)]
         arr_trj = create_torque_traj_from_x(G, const_torque_koef, 1, 0.1)
 
-        grab_obj_mat = chrono.ChMaterialSurfaceNSC()
-        grab_obj_mat.SetFriction(0.5)
-        grab_obj_mat.SetDampingF(0.1)
-        obj = chrono.ChBodyEasyBox(0.2, 0.2, 0.6, 1000, True, True, grab_obj_mat)
-        obj.SetCollide(True)
-        obj.SetPos(chrono.ChVectorD(0, 1.2, 0))
+        matich = DefaultChronoMaterial()
+        matich.Friction = 0.65
+        matich.DampingF = 0.65
+        obj = BlockWrapper(ChronoBodyEnv,
+                           shape=SimpleBody.ELLIPSOID,
+                           material=matich,
+                           pos=FrameTransform([0, 1, 0], [0, -0.048, 0.706, 0.706]))
 
         sim = step.SimulationStepOptimization(arr_trj, G, obj)
         sim.set_flags_stop_simulation(flags)
