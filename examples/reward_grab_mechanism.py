726f634c078677f2154c5d1a97d57e9e66119b22
MR-KNV <nasonov_kv@mail.ru>
2022-12-16 20:13:48 +0300

    Fix unused code

--- examples/reward_grab_mechanism.py
+++ examples/reward_grab_mechanism.py
@@ -1,14 +1,12 @@
 import random
 
 import pychrono as chrono
-from example_vocabulary import (B_NODES, J_NODES, LM_MOUNTS, PALM_LIST,
-                                RM_MOUNTS, get_terminal_graph_three_finger)
+from example_vocabulary import (B_NODES, J_NODES, LM_MOUNTS, RM_MOUNTS,
+                                get_terminal_graph_three_finger)
 
 import rostok.virtual_experiment.simulation_step as step
 from rostok.block_builder.basic_node_block import SimpleBody
-from rostok.block_builder.node_render import (ChronoBodyEnv,
-                                              DefaultChronoMaterial,
-                                              FrameTransform)
+from rostok.block_builder.node_render import (ChronoBodyEnv, DefaultChronoMaterial, FrameTransform)
 from rostok.criterion.criterion_calc import criterion_calc
 from rostok.criterion.flags_simualtions import FlagMaxTime
 from rostok.graph_grammar.node import BlockWrapper
