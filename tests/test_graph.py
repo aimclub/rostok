1f4095ba66526559061c9dbe42a7c71f3f35cc45
ZharkovKirill <labradudel13@gmail.com>
2023-01-12 18:10:15 +0300

    [tests] Move test graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,5 +1,8 @@
-from test_ruleset import get_terminal_graph_two_finger_mix, \
-get_terminal_graph_two_finger, get_terminal_graph_three_finger
+from test_ruleset import (get_terminal_graph_three_finger,
+                          get_terminal_graph_two_finger,
+                          get_terminal_graph_two_finger_mix, rule_vocab)
+
+from rostok.graph_grammar import make_random_graph
 
 
 def test_graph_equal():
@@ -8,3 +11,10 @@ def test_graph_equal():
     graph3 = get_terminal_graph_three_finger()
     assert graph_eq_1 != graph3
     assert graph_eq_1 == graph_eq_2
+
+
+def test_create_graph():
+    """
+        Test for graph grammar and rule
+    """
+    graph = make_random_graph.make_random_graph(5, rule_vocab)
