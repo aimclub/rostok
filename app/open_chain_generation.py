bf0259df4d0a7c11722961e058e67db7cfbf7cc6
Yefim Osipov <kraken.mix@gmail.com>
2023-01-17 15:05:59 +0300

    Create individual folder for launching

--- app/open_chain_generation.py
+++ app/open_chain_generation.py
@@ -1,7 +1,7 @@
 
-from rostok.api import OpenChainGen, create_generator_by_config
 from rostok.block_builder.basic_node_block import SimpleBody
+from rostok.launch.open_chain_gen import OpenChainGen, create_generator_by_config
 
-model = create_generator_by_config("rostok/config.ini")
+model = create_generator_by_config("rostok/launcher/config.ini")
 model.set_grasp_object(SimpleBody.SPHERE)
 graph_mechanism, trajectory, reward = model.run_generation()
