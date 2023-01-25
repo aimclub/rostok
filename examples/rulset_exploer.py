6ae0ca8a53aac654e1e1ef22e2c0d0da8fb385ff
ZharkovKirill <labradudel13@gmail.com>
2023-01-25 11:31:00 +0300

    remove unused var in example

--- examples/rulset_exploer.py
+++ examples/rulset_exploer.py
@@ -4,10 +4,7 @@ from example_vocabulary import rule_vocab
 from rostok.graph_grammar.graphgrammar_explorer import ruleset_explorer
 
 start = time.time()
-cool_list: set[GraphGrammar] = set()
-
 out = ruleset_explorer(3, rule_vocab)
-set_cool = set(cool_list)
 for i in out[0]:
     print(i.nodes(True))
 ex = time.time() - start
