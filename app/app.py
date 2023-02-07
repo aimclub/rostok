a2edaf8d29174977a676239d35a8ef61af6639ab
Mikhail Chaikovskii <chaykovskiym.e@gmail.com>
2023-02-07 16:59:48 +0300

    add new functionality for helper

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from pathlib import Path
+import sys
 import time
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import mcts
 # imports from standard libs
@@ -7,12 +9,16 @@ import networkx as nx
 # chrono imports
 import pychrono as chrono
 import rule_extention
-from control_optimisation import (create_grab_criterion_fun, create_traj_fun, get_object_to_grasp)
+from control_optimisation import (create_grab_criterion_fun, create_traj_fun,
+                                  get_object_to_grasp)
 
-from rostok.graph_generators.mcts_helper import prepare_mcts_state_and_helper, make_mcts_step
-from rostok.criterion.flags_simualtions import (FlagMaxTime, FlagNotContact, FlagSlipout)
+from rostok.criterion.flags_simualtions import (FlagMaxTime, FlagNotContact,
+                                                FlagSlipout)
+from rostok.graph_generators.mcts_helper import (make_mcts_step,
+                                                 prepare_mcts_state_and_helper)
 from rostok.graph_grammar.node import GraphGrammar
-from rostok.trajectory_optimizer.control_optimizer import (ConfigRewardFunction, ControlOptimizer)
+from rostok.trajectory_optimizer.control_optimizer import (
+    ConfigRewardFunction, ControlOptimizer)
 
 
 def plot_graph(graph: GraphGrammar):
@@ -52,39 +58,53 @@ control_optimizer = ControlOptimizer(cfg)
 # %% Init mcts parameters
 
 # Hyperparameters mctss
-iteration_limit = 20
+base_iteration_limit = 50
 
 # Initialize MCTS
-searcher = mcts.mcts(iterationLimit=iteration_limit)
+#searcher = mcts.mcts(iterationLimit=iteration_limit)
 finish = False
 
-G = GraphGrammar()
-max_numbers_rules = 3
+initial_graph = GraphGrammar()
+max_numbers_rules = 20
 # Create graph environments for algorithm (not gym)
-graph_env = prepare_mcts_state_and_helper(G, rule_vocabul, control_optimizer, max_numbers_rules, Path("./results"))
+graph_env = prepare_mcts_state_and_helper(initial_graph, rule_vocabul, control_optimizer, max_numbers_rules,
+                                          Path("./results"))
 mcts_helper = graph_env.helper
 mcts_helper.report.non_terminal_rules_limit = max_numbers_rules
-mcts_helper.report.search_parameter = iteration_limit
+mcts_helper.report.search_parameter = base_iteration_limit
 n_steps = 0
 #%% Run first algorithm
 start = time.time()
 while not finish:
+    iteration_limit = base_iteration_limit - int(graph_env.counter_action/max_numbers_rules * (base_iteration_limit*0.7))
+    searcher = mcts.mcts(iterationLimit=iteration_limit)
     finish, graph_env = make_mcts_step(searcher, graph_env, n_steps)
     n_steps += 1
     print(f"number iteration: {n_steps}, counter actions: {graph_env.counter_action} " +
           f"reward: {mcts_helper.report.get_best_info()[1]}")
 ex = time.time() - start
 print(f"time :{ex}")
+# saving results of the search
 report = mcts_helper.report
-report.draw_best_graph()
-best_graph, reward, best_control = mcts_helper.report.get_best_info()
-func_reward = control_optimizer.create_reward_function(best_graph)
-res = -func_reward(best_control)
-print("Best reward obtained in the MCTS search:", res)
-report.make_time_dependent_path()
+path = report.make_time_dependent_path()
 report.save()
 report.save_visuals()
 report.save_lists()
-print(len(report.seen_graphs.graph_list))
-print(len(report.seen_states.state_list))
-report.plot_means()
+report.save_means()
+# additions to the file
+with open(Path(path, "mcts_result.txt"), "a") as file:
+    gb_params = get_object_to_grasp().kwargs
+    original_stdout = sys.stdout
+    sys.stdout = file
+    print()
+    print("Object to grasp:", gb_params.get("shape"))
+    print("Object initial coordinats:", gb_params.get("pos"))
+    sys.stdout = original_stdout   
+
+# visualisation in the end of the search
+best_graph, reward, best_control = mcts_helper.report.get_best_info()
+func_reward = control_optimizer.create_reward_function(best_graph)
+res = -func_reward(best_control, True)
+print("Best reward obtained in the MCTS search:", res)
+#report.plot_means()
+#report.draw_best_graph()
