3b1259d5002497ad0ca305b1f6d6f5617fc17542
MR-KNV <nasonov_kv@mail.ru>
2023-02-13 15:16:01 +0300

    Change example

--- examples/reward_grab_mechanism.py
+++ examples/reward_grab_mechanism.py
@@ -8,7 +8,7 @@ import rostok.virtual_experiment.simulation_step as step
 from rostok.block_builder.envbody_shapes import Box
 from rostok.block_builder.node_render import (ChronoBodyEnv, DefaultChronoMaterial, FrameTransform)
 from rostok.criterion.criterion_calc import criterion_calc
-from rostok.criterion.flags_simualtions import FlagMaxTime
+from rostok.criterion.flags_simualtions import FlagMaxTime, FlagNotContact, FlagSlipout
 from rostok.graph_grammar.node import BlockWrapper
 from rostok.graph_grammar.nodes_division import nodes_division, sort_left_right
 from rostok.trajectory_optimizer.control_optimizer import num_joints
@@ -16,7 +16,7 @@ from rostok.trajectory_optimizer.trajectory_generator import \
     create_torque_traj_from_x
 
 # Constants
-MAX_TIME = 1
+MAX_TIME = 2
 TIME_STEP = 1e-3
 
 # Graph initialization
@@ -24,8 +24,10 @@ graph = get_terminal_graph_three_finger()
 
 # Create trajectory
 number_trq = num_joints(graph)
-const_torque_koef = [random.random() for _ in range(number_trq)]
+# const_torque_koef = [random.random() for _ in range(number_trq)]
+const_torque_koef = [0, 0, 0, 0, -1, 6]
 arr_trj = create_torque_traj_from_x(graph, const_torque_koef, MAX_TIME, TIME_STEP)
+
 
 # Create object to grasp with material props
 mat = DefaultChronoMaterial()
@@ -34,11 +36,11 @@ mat.DampingF = 0.65
 obj = BlockWrapper(ChronoBodyEnv,
                    shape=Box(),
                    material=mat,
-                   pos=FrameTransform([0, 1, 0], [0, -0.048, 0.706, 0.706]))
+                   pos=FrameTransform([0, 0.4, 0], [0, -0.048, 0.706, 0.706]))
 
 # Configurate simulation
 config_sys = {"Set_G_acc": chrono.ChVectorD(0, 0, 0)}
-flags = [FlagMaxTime(MAX_TIME)]
+flags = [FlagMaxTime(MAX_TIME), FlagNotContact(1), FlagSlipout(0.5, 0.5)]
 
 sim = step.SimulationStepOptimization(arr_trj, graph, obj)
 sim.set_flags_stop_simulation(flags)
@@ -48,7 +50,7 @@ sim.change_config_system(config_sys)
 sim_output = sim.simulate_system(TIME_STEP, True)
 
 # Weight coefficients for reward function
-WEIGHTS = [1, 5, 2]
+WEIGHTS = [5, 10, 1]
 # Time to grasp object
 GAIT_PERIOD = 2.5
 
