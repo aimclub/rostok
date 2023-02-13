f3ed3c7d32cf3dfab4cdbfbec370fa1e647516aa
MR-KNV <nasonov_kv@mail.ru>
2023-02-13 15:17:35 +0300

    Fix criterions

--- rostok/criterion/criterion_calc.py
+++ rostok/criterion/criterion_calc.py
@@ -45,19 +45,19 @@ def criterion_calc(sim_output, b_nodes, j_nodes, lb_nodes, rb_nodes, weights, ga
         reward (float): Reward for grasping device
     """
 
-    [b_nodes_sim, j_nodes_sim, lb_nodes_sim,
-     rb_nodes_sim] = traj_to_list(b_nodes, j_nodes, lb_nodes, rb_nodes, sim_output)
+    [_, j_nodes_sim, _,
+     _] = traj_to_list(b_nodes, j_nodes, lb_nodes, rb_nodes, sim_output)
 
     #1) Force criterion
     if np.size(sim_output[-1].obj_contact_forces) == 0 or np.median(
-            sim_output[-1].obj_amount_surf_forces) < 6:
+            sim_output[-1].obj_amount_surf_forces) < 4:
         force_crit = 0
     else:
         force_crit = 1 / (1 + np.mean(sim_output[-1].obj_contact_forces))
 
     #2) Time criterion
     if np.size(j_nodes_sim) > 0:
-        time_crit = j_nodes_sim[0]['time'][-1] / gait
+        time_crit = np.square(j_nodes_sim[0]['time'][-1])
     else:
         time_crit = 0
         
@@ -67,10 +67,16 @@ def criterion_calc(sim_output, b_nodes, j_nodes, lb_nodes, rb_nodes, weights, ga
     if np.size(sim_output[-1].obj_cont_coord) > 0:
        for idx,_ in enumerate(sim_output[-1].obj_cont_coord):
             dist_list.append(distance.euclidean(sim_output[-1].obj_cont_coord[idx], sim_output[-1].obj_COG[idx]))
-    cog_crit = 1/(1+np.mean(dist_list))
+    if np.size(dist_list) > 0:
+        cog_crit = 1/(1+np.mean(dist_list))
+    else:
+        cog_crit = 0
 
 
-    if np.size(sim_output[-1].obj_contact_forces) == 0:
-        return 0
+    # if np.size(sim_output[-1].obj_contact_forces) == 0:
+    #     return 0
+    if force_crit == 0:
+        return 0.5*(-weights[0] * force_crit - weights[1] * time_crit- weights[2] * cog_crit)
     else:
         return -weights[0] * force_crit - weights[1] * time_crit- weights[2] * cog_crit
+    # return -weights[0] * force_crit - weights[1] * time_crit- weights[2] * cog_crit
