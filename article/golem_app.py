b8cceb75fb6d716f61cc77a192873b8becfebc9d
ZharkovKirill <labradudel13@gmail.com>
2023-02-25 17:02:18 +0300

    GOLEM kill am all

--- article/golem_app.py
+++ article/golem_app.py
@@ -20,7 +20,7 @@ from golem.core.optimisers.optimizer import GraphGenerationParams
 from obj_grasp.objects import get_obj_hard_ellipsoid, get_object_to_grasp_sphere
 from optmizers_config import get_cfg_graph
 from rule_sets.ruleset_old_style_graph import create_rules
-
+from rule_sets.rule_extention_golem_edition import rule_vocab, torque_dict
 from rostok.adapters.golem_adapter import (GraphGrammarAdapter,
                                            GraphGrammarFactory)
 from rostok.graph_grammar.graph_utils import plot_graph
@@ -30,9 +30,9 @@ from rostok.trajectory_optimizer.control_optimizer import ControlOptimizer
 import random
 from mutation_logik import add_mut, del_mut
 from golem.core.optimisers.genetic.operators import crossover
+import init_pop
 
-
-rule_vocab, torque_dict = create_rules()
+#rule_vocab, torque_dict = create_rules()
 node_vocab = rule_vocab.node_vocab
 def custom_metric(graph: GraphGrammar):
     existing_variables_num = -len(graph)
@@ -51,33 +51,35 @@ adapter_local = GraphGrammarAdapter()
 
 rule_vocab = deepcopy(rule_vocab)
 init_population_gr = []
-for _ in range(120):
-    rand_mech = make_random_graph(12, rule_vocab)
+for _ in range(15):
+    rand_mech = make_random_graph(5, rule_vocab)
     init_population_gr.append(rand_mech)
 
+init_population_gr = init_pop.get_population_zoo()
 initial = adapter_local.adapt(init_population_gr)
 cfg = get_cfg_graph(torque_dict)
 cfg.get_rgab_object_callback = get_object_to_grasp_sphere
 optic = ControlOptimizer(cfg)
 
 build_wrapperd  = partial(custom_metriwith_build_mechs, optimizer = optic)
-objective = Objective({'custom': build_wrapperd})
+name_objective = cfg.get_rgab_object_callback.__name__
+objective = Objective({name_objective: build_wrapperd})
 objective_eval = ObjectiveEvaluate(objective)
-timeout = datetime.timedelta(hours = 8)
+timeout = datetime.timedelta(hours = 4)
 
 terminal_nodes = [i for i in list(node_vocab.node_dict.values()) if  i.is_terminal]
 nodes_types = adapter_local.adapt_node_seq(terminal_nodes)
 
 def custom_mutation_add(graph: GraphGrammar, **kwargs) -> GraphGrammar:
     try:
-        graph_mut = add_mut(graph, terminal_nodes)
+        graph_mut = add_mut(graph, terminal_nodes, (0.7, 0.5, 0.5 ))
     except:
         graph_mut = deepcopy(graph)
   
     return graph_mut
 def custom_mutation_del(graph: GraphGrammar, **kwargs) -> GraphGrammar:
     try:
-        graph_mut = del_mut(graph, terminal_nodes)
+        graph_mut = del_mut(graph, terminal_nodes, (0.5, 1, 0.2 ))
     except:
         graph_mut = deepcopy(graph)
     return graph_mut
@@ -91,8 +93,10 @@ graph_generation_params = GraphGenerationParams(
 
 
 requirements = GraphRequirements(
-    max_arity=4,
+    max_arity=5,
+    min_arity=1,
     max_depth=17,
+    early_stopping_timeout = None,
     parallelization_mode="single",
     timeout=timeout,
     num_of_generations=40,
@@ -100,23 +104,14 @@ requirements = GraphRequirements(
     history_dir=None)
 
 optimizer_parameters = GPAlgorithmParameters(
-    pop_size = 120,
-    max_pop_size = 120,
-    
-
-    crossover_prob=0.5, mutation_prob=0.7,
+    pop_size = len(initial),
+    max_pop_size = len(initial)+10,
+    crossover_prob=0.5, mutation_prob=0.8,
     genetic_scheme_type=GeneticSchemeTypesEnum.parameter_free,
         mutation_types=[
-            #MutationTypesEnum.growth,
             MutationTypesEnum.none,
-            #MutationTypesEnum.simple,
-            #MutationTypesEnum.growth,
-            #MutationTypesEnum.single_edge,
-            #MutationTypesEnum.single_add,
             custom_mutation_add,
             custom_mutation_del,
-            #MutationTypesEnum.single_add,
-            #MutationTypesEnum.single_add,
         ],
     crossover_types = [CrossoverTypesEnum.gg_subtree],
     regularization_type = RegularizationTypesEnum.none,
---
+++ article/init_pop.py
@@ -0,0 +1,106 @@
+import datetime
+from copy import deepcopy
+from functools import partial
+import pickle
+import time
+from golem.core.dag.verification_rules import DEFAULT_DAG_RULES
+from golem.core.optimisers.genetic.gp_optimizer import EvoGraphOptimizer
+from golem.core.optimisers.genetic.gp_params import GPAlgorithmParameters
+from golem.core.optimisers.genetic.operators.crossover import \
+    CrossoverTypesEnum
+from golem.core.optimisers.genetic.operators.inheritance import \
+    GeneticSchemeTypesEnum
+from golem.core.optimisers.genetic.operators.mutation import (
+    MutationStrengthEnum, MutationTypesEnum)
+from golem.core.optimisers.genetic.operators.regularization import \
+    RegularizationTypesEnum
+from golem.core.optimisers.objective import Objective, ObjectiveEvaluate
+from golem.core.optimisers.optimization_parameters import GraphRequirements
+from golem.core.optimisers.optimizer import GraphGenerationParams
+from obj_grasp.objects import get_obj_hard_ellipsoid, get_object_to_grasp_sphere
+from optmizers_config import get_cfg_graph
+from rule_sets.ruleset_old_style_graph import create_rules
+from rule_sets.rule_extention_golem_edition import rule_vocab, torque_dict
+from rostok.adapters.golem_adapter import (GraphGrammarAdapter,
+                                           GraphGrammarFactory)
+from rostok.graph_grammar.graph_utils import plot_graph
+from rostok.graph_grammar.make_random_graph import make_random_graph
+from rostok.graph_grammar.node import GraphGrammar
+from rostok.trajectory_optimizer.control_optimizer import ControlOptimizer
+import random
+from mutation_logik import add_mut, del_mut
+from golem.core.optimisers.genetic.operators import crossover
+
+def get_non_terminal_one_finger():
+    one_finger = GraphGrammar()
+    one = rule_vocab.get_rule("InitMechanism_1")
+    upper = rule_vocab.get_rule("FingerUpper")
+    one_finger.apply_rule(one)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    return one_finger
+
+def get_non_terminal_two_finger_asym():
+    one_finger = GraphGrammar()
+    one = rule_vocab.get_rule("InitMechanism_2")
+    upper = rule_vocab.get_rule("FingerUpper")
+    terminal_end = rule_vocab.get_rule("TerminalEndLimb1")
+    one_finger.apply_rule(one)
+    one_finger.apply_rule(upper)
+    
+    one_finger.apply_rule(terminal_end)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    return one_finger
+
+def get_non_terminal_three_finger_short():
+    one_finger = GraphGrammar()
+    one = rule_vocab.get_rule("InitMechanism_3_R_A")
+    upper = rule_vocab.get_rule("FingerUpper")
+    one_finger.apply_rule(one)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    return one_finger
+
+def get_non_terminal_three_finger_long():
+    one_finger = GraphGrammar()
+    one = rule_vocab.get_rule("InitMechanism_3_L")
+    upper = rule_vocab.get_rule("FingerUpper")
+    one_finger.apply_rule(one)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    return one_finger
+
+def get_non_terminal_four_finger():
+    one_finger = GraphGrammar()
+    one = rule_vocab.get_rule("InitMechanism_4_A")
+    upper = rule_vocab.get_rule("FingerUpper")
+    one_finger.apply_rule(one)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    one_finger.apply_rule(upper)
+    return one_finger
+
+STRUCTURE_ZOO = [get_non_terminal_one_finger, get_non_terminal_two_finger_asym, get_non_terminal_three_finger_short, get_non_terminal_three_finger_long, get_non_terminal_four_finger]
+def get_population_zoo():
+    zoo = []
+    for struct in STRUCTURE_ZOO:
+        
+        graphs_one_struct = []
+        for _ in range(3):
+            graph_zoo = struct()
+            rule_vocab.make_graph_terminal(graph_zoo)
+            graphs_one_struct.append(graph_zoo)
+        zoo.extend(graphs_one_struct)
+    return zoo
