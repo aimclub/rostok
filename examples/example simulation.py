66ee667c2778b1a80f4f807965ddf6dc6e19d457
Mikhail Chaikovskii <chaykovskiym.e@gmail.com>
2023-04-17 19:04:09 +0300

    update RobotSimulation

--- examples/example simulation.py
+++ examples/example simulation.py
@@ -1,9 +1,9 @@
-import random
+import numpy as np
 
 import pychrono as chrono
 from example_vocabulary import (get_terminal_graph_no_joints, get_terminal_graph_three_finger,
                                 get_terminal_graph_two_finger)
-
+from rostok.graph_grammar.node_block_typing import get_joint_vector_from_graph
 import rostok.virtual_experiment.simulation_step as step
 from rostok.block_builder_api.block_parameters import DefaultFrame, Material, FrameTransform
 from rostok.block_builder_api.block_blueprints import EnvironmentBodyBlueprint
@@ -12,25 +12,31 @@ from rostok.trajectory_optimizer.control_optimizer import num_joints
 from rostok.trajectory_optimizer.trajectory_generator import \
     create_torque_traj_from_x
 from rostok.block_builder_api.easy_body_shapes import Box
-from rostok.simulation_chrono.basic_simulation import SystemPreview
+from rostok.simulation_chrono.basic_simulation import SystemPreview, RobotSimulationChrono
+from rostok.block_builder_chrono.block_builder_chrono_api import ChronoBlockCreatorInterface as creator
 mechs = [
-    get_terminal_graph_three_finger, get_terminal_graph_no_joints, get_terminal_graph_two_finger
+    get_terminal_graph_three_finger, get_terminal_graph_two_finger
 ]
 
+def rotation_x(alpha):
+    quat_X_ang_alpha = chrono.Q_from_AngX(np.deg2rad(alpha))
+    return [quat_X_ang_alpha.e0, quat_X_ang_alpha.e1, quat_X_ang_alpha.e2, quat_X_ang_alpha.e3]
 for get_graph in mechs:
-    # Constants
-    MAX_TIME = 2
-    TIME_STEP = 1e-3
 
     graph = get_graph()
-    sim = SystemPreview()
+    print(get_joint_vector_from_graph(graph))
+    controll_parameters = np.random.randint(1,15,len(get_joint_vector_from_graph(graph)))
+    print(controll_parameters)
+
+    sim = RobotSimulationChrono([])
 
     # Create object to grasp
     mat = Material()
     mat.Friction = 0.65
     mat.DampingF = 0.65
-    obj = EnvironmentBodyBlueprint(shape = Box(0.1,0.2,0.5),material=mat,
+
+    obj = EnvironmentBodyBlueprint(shape = Box(3,0.2,3),material=mat,
                                    pos=FrameTransform([0, -0.4, 0], [1,0,0,0]))
-    sim.add_object(obj)
-    sim.add_design(graph)
-    sim.simulate(10000, True)
+    sim.add_object(creator.init_block_from_blueprint(obj))
+    sim.add_design(graph, controll_parameters,FrameTransform([0, 2.5, 0], rotation_x(180)))
+    sim.simulate(10000, 0.01, 10, True)
--- rostok/graph_grammar/node_block_typing.py
+++ rostok/graph_grammar/node_block_typing.py
@@ -1,5 +1,7 @@
-from rostok.graph_grammar.node import Node
-from rostok.block_builder_api.block_blueprints import  BodyBlueprintType, JointBlueprintType, TransformBlueprintType
+from rostok.block_builder_api.block_blueprints import (BodyBlueprintType,
+                                                       JointBlueprintType,
+                                                       TransformBlueprintType)
+from rostok.graph_grammar.node import GraphGrammar, Node
 
 
 class NodeFeatures:
@@ -14,3 +16,15 @@ class NodeFeatures:
     @staticmethod
     def is_transform(node: Node):
         return issubclass(type(node.block_blueprint), TransformBlueprintType)
+
+def get_joint_vector_from_graph(graph:GraphGrammar):
+    joint_vector = []
+    paths = graph.get_sorted_root_based_paths()
+    for path in paths:
+        for idx in path:
+            if idx in joint_vector:
+                continue
+            else:
+                if NodeFeatures.is_joint(graph.get_node_by_id(idx)):
+                    joint_vector.append(idx)
+    return joint_vector
