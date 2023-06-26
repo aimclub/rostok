bb0471b4932431a0692486c0d88204496e88c6e3
Mikhail Chaikovskii <chaykovskiym.e@gmail.com>
2023-06-26 15:35:27 +0300

    add rotation in example

--- examples/example_simulation_preview.py
+++ examples/example_simulation_preview.py
@@ -11,7 +11,13 @@ from rostok.library.obj_grasp.objects import (get_obj_hard_mesh_piramida, get_ob
                                               get_object_parametrized_sphere)
 from rostok.library.rule_sets.simple_designs import (get_palm, get_two_link_one_finger)
 from rostok.simulation_chrono.basic_simulation import SystemPreviewChrono
-from rostok.simulation_chrono.simulation_utils import set_covering_sphere_based_position
+from rostok.simulation_chrono.simulation_utils import \
+    set_covering_sphere_based_position
+
+
+def rotation_x(alpha):
+    quat_X_ang_alpha = chrono.Q_from_AngX(np.deg2rad(alpha))
+    return [quat_X_ang_alpha.e0, quat_X_ang_alpha.e1, quat_X_ang_alpha.e2, quat_X_ang_alpha.e3]
 
 
 graph = get_terminal_graph_three_finger()
