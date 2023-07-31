8a4e77292db8bc4cf062f8a4fb2fe337f8c52aca
Yefim Osipov <kraken.mix@gmail.com>
2023-07-31 14:12:30 +0300

    Fix exmaple

--- examples/example_optimisation_multiobject.py
+++ examples/example_optimisation_multiobject.py
@@ -2,10 +2,13 @@ from copy import deepcopy
 from rostok.block_builder_chrono.block_builder_chrono_api import \
     ChronoBlockCreatorInterface as creator
 from rostok.block_builder_chrono.blocks_utils import frame_transform_to_chcoordsys
-from rostok.criterion.criterion_calculation import (ForceCriterion, LateForceAmountCriterion,
-                                                    LateForceCriterion, ObjectCOGCriterion,
-                                                    SimulationReward, TimeCriterion)
-from rostok.criterion.simulation_flags import (FlagContactTimeOut, FlagFlyingApart, FlagSlipout)
+from rostok.criterion.criterion_calculation import (ForceCriterion, SimulationReward, TimeCriterion,
+                                                    EventSlipOut, InstantContactingLinkCriterion,
+                                                    InstantForceCriterion,
+                                                    InstantObjectCOGCriterion, GraspTimeCriterion,
+                                                    FinalPositionCriterion)
+from rostok.criterion.simulation_flags import (EventContact, EventContactTimeOut, EventFlyingApart,
+                                               EventStopExternalForce, EventGrasp)
 from rostok.simulation_chrono.simulation_scenario import ConstTorqueGrasp
 from rostok.trajectory_optimizer.control_optimizer import (CalculatorWithGraphOptimization,
                                                            CalculatorWithOptimizationDirect,
@@ -24,10 +27,22 @@ def config_with_standard_multiobject(grasp_object_blueprint: list[EnvironmentBod
     # configurate the simulation manager
     simulation_manager = ConstTorqueGrasp(0.001, 3)
 
-    simulation_manager.add_flag(FlagContactTimeOut(1))
-    simulation_manager.add_flag(FlagFlyingApart(10))
-
-    simulation_manager.add_flag(FlagSlipout(0.8))
+    simulation_manager = ConstTorqueGrasp(0.001, 3)
+    simulation_manager.grasp_object_callback = lambda: creator.create_environment_body(
+        grasp_object_blueprint)
+    event_contact = EventContact()
+    simulation_manager.add_event(event_contact)
+    event_timeout = EventContactTimeOut(2, event_contact)
+    simulation_manager.add_event(event_timeout)
+    event_flying_apart = EventFlyingApart(4)
+    simulation_manager.add_event(event_flying_apart)
+    event_slipout = EventSlipOut(0.5)
+    simulation_manager.add_event(event_slipout)
+    event_grasp = EventGrasp(
+        grasp_limit_time=4,
+        contact_event=event_contact,
+        simulation_stop=0,
+    )
     simulation_managers = []
     object_callback = [
         (lambda obj=obj: creator.create_environment_body(obj)) for obj in grasp_object_blueprint
@@ -39,11 +54,15 @@ def config_with_standard_multiobject(grasp_object_blueprint: list[EnvironmentBod
     #create criterion manager
     simulation_rewarder = SimulationReward()
     #create criterions and add them to manager
-    simulation_rewarder.add_criterion(TimeCriterion(1), 1)
-    simulation_rewarder.add_criterion(ForceCriterion(), 1)
-    simulation_rewarder.add_criterion(ObjectCOGCriterion(), 1)
-    simulation_rewarder.add_criterion(LateForceCriterion(0.5, 3), 1)
-    simulation_rewarder.add_criterion(LateForceAmountCriterion(0.5), 1)
+    simulation_rewarder.add_criterion(TimeCriterion(10, event_timeout, event_grasp), 1)
+    simulation_rewarder.add_criterion(ForceCriterion(event_timeout), 1)
+    simulation_rewarder.add_criterion(InstantContactingLinkCriterion(event_grasp), 1)
+    simulation_rewarder.add_criterion(InstantForceCriterion(event_grasp), 1)
+    simulation_rewarder.add_criterion(InstantObjectCOGCriterion(event_grasp), 1)
+    n_steps = int(10 / 0.001)
+    print(n_steps)
+    simulation_rewarder.add_criterion(GraspTimeCriterion(event_grasp, n_steps), 1)
+    simulation_rewarder.add_criterion(FinalPositionCriterion(20, event_grasp, event_slipout), 1)
 
     control_optimizer = TendonLikeControlOptimization(simulation_managers, simulation_rewarder,
                                                       (3, 15), 1)
