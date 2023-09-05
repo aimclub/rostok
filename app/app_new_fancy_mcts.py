90fc59bc37bba493a684796b69e6b9d50923c97b
ZharkovKirill <rubekon13@gmail.com>
2023-09-05 12:34:53 +0300

    prekol

---
+++ app/app_new_fancy_mcts.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from rostok.graph_generators.environments.design_environment import DesignEnvironment, SubDesignEnvironment
+from rostok.graph_generators.search_algorithms.mcts import MCTS
+from rostok.graph_generators.search_algorithms.random_search import RandomSearch
+
+from rostok.library.rule_sets.ruleset_old_style_smc import create_rules
+from rostok.graph_grammar.node import GraphGrammar
+from rostok.library.obj_grasp.objects import get_object_parametrized_sphere, get_object_parametrized_sphere_smc
+import sys
+ 
+from mcts_run_setup import config_combination_force_tendon_multiobject
+
+rule_vocabulary = create_rules()
+grasp_object_blueprint = []
+grasp_object_blueprint.append(get_object_parametrized_sphere(0.05))
+grasp_object_blueprint.append(get_object_parametrized_sphere(0.03))
+grasp_object_blueprint.append(get_object_parametrized_sphere(0.035))
+# create reward counter using run setup function
+control_optimizer = config_combination_force_tendon_multiobject(grasp_object_blueprint, [ 1, 1, 1])
+
+init_graph = GraphGrammar()
+env = SubDesignEnvironment(rule_vocabulary, control_optimizer, 5, init_graph, 2)
+
+mcts = MCTS(env)
+
+state = env.initial_state
+trajectory = [state]
+while not env.is_terminal_state(state)[0]:
+    for __ in range(10):
+        mcts.search(state)
+    
+    pi = mcts.get_policy(state)
+    a = max(env.actions, key=lambda x: pi[x])
+    state, reward, is_terminal_state, __ = env.next_state(state, a)
+    print(f"State: {state}, Reward: {reward}, is_terminal_state: {is_terminal_state}")
+    trajectory.append(state)
+env.save_environment("test")
+mcts.save("test")
+print(f"Trajectory: {trajectory}")
