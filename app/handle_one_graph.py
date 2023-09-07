ad5ce95497818d81ea0fbf114fadfa98477d8354
Mikhail Chaikovskii <chaykovskiym.e@gmail.com>
2023-09-08 01:07:28 +0300

    force_data

--- app/handle_one_graph.py
+++ app/handle_one_graph.py
@@ -1,18 +1,13 @@
-import sys
-import time
-from datetime import datetime
-from pathlib import Path
 
 import hyperparameters as hp
 import matplotlib.pyplot as plt
-import mcts
+
 import numpy as np
 from mcts_run_setup import config_with_const_troques, config_with_tendon
 
-from rostok.graph_grammar.node import GraphGrammar
 from rostok.library.obj_grasp.objects import (get_object_ellipsoid, get_object_parametrized_sphere,
                                               get_object_parametrized_sphere_smc)
-from rostok.library.rule_sets.ruleset_simple_fingers import create_rules
+
 from rostok.library.rule_sets.simple_designs import (
     get_two_link_three_finger, get_two_same_link_one_finger, get_four_same_link_one_finger,
     get_three_same_link_one_finger, get_two_link_three_finger_rotated, get_three_link_three_finger)
@@ -48,7 +43,7 @@ graph = get_three_link_three_finger()
 control = [10, 10, 10]
 
 data = control_optimizer.optim_parameters2data_control(control, graph)
-
+control_optimizer.data.create_pulley_data_file = True
 vis = True
 #simulation_output: SimulationResult = simulation_manager.run_simulation(graph, data, [[-45.0, 0.0],[-45,0],[-45,0]], vis, True)
 simulation_output: SimulationResult = simulation_manager.run_simulation(
