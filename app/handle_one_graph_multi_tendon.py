6eec25d7477d6065b807c5da89a0652035dd7138
Mikhail Chaikovskii <chaykovskiym.e@gmail.com>
2023-09-09 22:26:13 +0300

    parameters

--- app/handle_one_graph_multi_tendon.py
+++ app/handle_one_graph_multi_tendon.py
@@ -32,10 +32,10 @@ simulation_manager = control_optimizer.simulation_scenario
 
 graph = get_two_link_three_finger()
 # graph = get_two_same_link_one_finger()
-graph = get_four_same_link_one_finger()
-graph = get_three_same_link_one_finger()
+# graph = get_four_same_link_one_finger()
+# graph = get_three_same_link_one_finger()
 
-control = [[0]]
+control = [[10, 10, 10]]
 
 data = control_optimizer.optim_parameters2data_control(control, graph)[0]
 
