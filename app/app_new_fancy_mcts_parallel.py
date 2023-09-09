48353a050b66fc50156be96fccffebae4fbfa4d0
Yefim Osipov <kraken.mix@gmail.com>
2023-09-09 16:28:13 +0300

    add real objects

--- app/app_new_fancy_mcts_parallel.py
+++ app/app_new_fancy_mcts_parallel.py
@@ -15,9 +15,9 @@ from mcts_run_setup import config_combination_force_tendon_multiobject_parallel
 if __name__ == "__main__":
     rule_vocabulary = create_rules()
     grasp_object_blueprint = []
-    grasp_object_blueprint.append(get_object_ellipsoid(0.10, 0.08, 0.14, 10))
-    grasp_object_blueprint.append(get_object_cylinder(0.07, 0.09, 0))
-    grasp_object_blueprint.append(get_object_box(0.12, 0.12, 0.1, 0))
+    grasp_object_blueprint.append()
+    grasp_object_blueprint.append(get_object_ellipsoid(0.14, 0.14, 0.22, 0, mass = 0.188))
+    grasp_object_blueprint.append(get_object_box(0.155, 0.127, 0.088*2, 0, mass = 0.176))
     # create reward counter using run setup function
     control_optimizer = config_combination_force_tendon_multiobject_parallel(grasp_object_blueprint, [1, 1, 1])
 
