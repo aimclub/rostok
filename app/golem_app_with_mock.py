ac7c7c0e476a0ac8b31d72797380c4feec0c66eb
ZharkovKirill <rubekon13@gmail.com>
2023-09-28 14:08:54 +0300

    3

--- app/golem_app_with_mock.py
+++ app/golem_app_with_mock.py
@@ -3,34 +3,40 @@ import pickle
 import time
 from golem.core.dag.verification_rules import DEFAULT_DAG_RULES
 from golem.core.optimisers.genetic.gp_optimizer import EvoGraphOptimizer
-from golem.core.optimisers.genetic.gp_params import GPAlgorithmParameters
+from golem.core.optimisers.genetic.gp_params import GPAlgorithmParameters, MutationAgentTypeEnum
 from golem.core.optimisers.genetic.operators.inheritance import \
     GeneticSchemeTypesEnum
-from golem.core.optimisers.genetic.operators.base_mutations import (MutationStrengthEnum, tree_growth)
+from golem.core.optimisers.genetic.operators.base_mutations import (MutationStrengthEnum,
+                                                                    tree_growth)
 from golem.core.optimisers.genetic.operators.regularization import \
     RegularizationTypesEnum
-from golem.core.optimisers.genetic.operators import base_mutations
 from golem.core.optimisers.objective import Objective, ObjectiveEvaluate
 from golem.core.optimisers.optimization_parameters import GraphRequirements
 from golem.core.optimisers.optimizer import GraphGenerationParams
 
 from rostok.adapters.golem_adapter import (GraphGrammarAdapter, GraphGrammarFactory)
- 
+
 from rostok.graph_grammar.crossovers import subtree_crossover
-from preapare_evo import load_init_population, terminal_nodes, custom_mutation_add, custom_mutation_del, create_balance_population, rule_vocab
+from preapare_evo import custom_mutation_del_body, load_init_population, terminal_nodes, \
+create_balance_population, rule_vocab, custom_mutation_del_j, custom_mutation_del_tr, \
+custom_mutation_add_body, custom_mutation_add_j, custom_mutation_add_tr
 from evaluate_graph import mock_with_build_mech
-from mcts_run_setup import config_with_standard_graph 
+from mcts_run_setup import config_with_standard_graph
 from rostok.library.obj_grasp.objects import get_object_box
 from rostok.library.rule_sets import ruleset_old_style_graph
 
 boxer = get_object_box(1.2, 0.5, 0.8, 0)
 rules, torque_dict = ruleset_old_style_graph.create_rules()
 optic = config_with_standard_graph(boxer, torque_dict)
+
+
 def fun(x):
     res = optic.calculate_reward(x)
     return -res[0]
+
+
 CACHED_POPULATION = True
-TIMEOUT_MINUTES = 60*2
+TIMEOUT_MINUTES = 60 * 2
 
 # Define objective
 name_objective = mock_with_build_mech.__name__
@@ -55,24 +61,27 @@ graph_generation_params = GraphGenerationParams(adapter=adapter_local,
                                                 node_factory=GraphGrammarFactory(terminal_nodes))
 
 timeout = datetime.timedelta(minutes=TIMEOUT_MINUTES)
-requirements = GraphRequirements(max_arity=5,
-                                 min_arity=1,
-                                 max_depth=17,
-                                 # parallelization_mode='single',
-                                 n_jobs=4,
-                                 timeout=timeout,
-                                 num_of_generations=60,
-                                 early_stopping_iterations=30,
-                                 history_dir=None,
-                                 keep_n_best=3)
+requirements = GraphRequirements(
+    max_arity=5,
+    min_arity=1,
+    max_depth=17,
+    # parallelization_mode='single',
+    n_jobs=4,
+    timeout=timeout,
+    num_of_generations=60,
+    early_stopping_iterations=30,
+    history_dir=None,
+    keep_n_best=3)
 
 optimizer_parameters = GPAlgorithmParameters(
     pop_size=len(adapted_initial_population),
     max_pop_size=len(adapted_initial_population) + 10,
     crossover_prob=0.5,
     mutation_prob=0.5,
+    adaptive_mutation_type = MutationAgentTypeEnum.default, 
     genetic_scheme_type=GeneticSchemeTypesEnum.parameter_free,
-    mutation_types=[custom_mutation_add, custom_mutation_del],
+    mutation_types=[custom_mutation_del_body, custom_mutation_del_j, custom_mutation_del_tr,  
+                    custom_mutation_add_body, custom_mutation_add_j, custom_mutation_add_tr],
     crossover_types=[subtree_crossover],
     regularization_type=RegularizationTypesEnum.none,
     mutation_strength=MutationStrengthEnum.mean)
