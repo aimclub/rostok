61a37ae1d2a56ef063a1bdf0eeeca363137db21e
ZharkovKirill <rubekon13@gmail.com>
2023-09-28 17:50:12 +0300

    COLLL CASES

--- app/collision_case.py
+++ app/collision_case.py
@@ -3,7 +3,7 @@ import hyperparameters as hp
 
 from mcts_run_setup import config_with_standard_graph
 from rostok.graph_grammar.node_block_typing import get_joint_vector_from_graph
-from rostok.library.obj_grasp.objects import get_object_box, get_object_ellipsoid, get_object_parametrized_sphere
+from rostok.library.obj_grasp.objects import get_object_box, get_object_cylinder, get_object_ellipsoid, get_object_parametrized_sphere
 from rostok.library.rule_sets import ruleset_old_style_graph
 from rostok.library.rule_sets.simple_designs import (get_two_link_three_finger)
 from rostok.simulation_chrono.basic_simulation import SimulationResult
@@ -14,7 +14,8 @@ rules, torque_dict = ruleset_old_style_graph.create_rules()
 
 grasp_object_blueprint = get_object_parametrized_sphere(0.5)
 grasp_object_blueprint = get_object_box(1.2, 0.5, 0.8, 0)
-
+grasp_object_blueprint = get_object_ellipsoid(1.3, 0.9, 0.8, 0)
+grasp_object_blueprint = get_object_cylinder(0.55, 0.8,0)
 # create reward counter using run setup function
 control_optimizer = config_with_standard_graph(grasp_object_blueprint, torque_dict)
 
