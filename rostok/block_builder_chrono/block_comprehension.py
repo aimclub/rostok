47eddc3e11cb53a6cb491e7ffbe0426220e27896
MikhailChaikovskii <chaykovskiym.e@gmail.com>
2023-10-04 14:45:27 +0300

    format

--- rostok/block_builder_chrono/block_comprehension.py
+++ rostok/block_builder_chrono/block_comprehension.py
@@ -1,6 +1,9 @@
-from rostok.block_builder_chrono.block_builder_chrono_api import ChronoBlockCreatorInterface
- 
+from rostok.block_builder_chrono.block_builder_chrono_api import \
+    ChronoBlockCreatorInterface
+
+
 def calc_volume_body(blueprint):
+    """Calculate the volume of the body."""
     body = ChronoBlockCreatorInterface.init_block_from_blueprint(blueprint)
     volume = body.body.GetMass() / body.body.GetDensity()
     return volume
