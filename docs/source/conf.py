3ea1780ec4aee48b0e0353fbcf34a4008a6b1592
Yefim Osipov <kraken.mix@gmail.com>
2023-10-18 14:56:11 +0300

    Revert "Add readme"
    This reverts commit e27c2978097738ed0c635ad137a81fd05ab5552a.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../rostok'))
 
 #The master toctree document
